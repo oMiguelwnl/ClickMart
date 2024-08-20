d1872d5757a829559cdffe9e165fd6625ef4927b
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-08-20 15:58:47 -0300

    fix: resolve issue with Tailwind CSS not applying styles in Vite project

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./src/**/*.{html,js,jsx}"];
-export const mode = "jit";
-export const theme = {
-  fontFamily: {
-    Roboto: ["Roboto", "sans-serif"],
-    Poppins: ["Poppins", "sans-serif"],
-  },
-  extend: {
-    screens: {
-      "1000px": "1050px",
-      "1100px": "1110px",
-      "800px": "800px",
-      "1300px": "1300px",
-      "400px": "400px",
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",
+  theme: {
+    fontFamily: {
+      Roboto: ["Roboto", "sans-serif"],
+      Poppins: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      screens: {
+        "1000px": "1050px",
+        "1100px": "1110px",
+        "800px": "800px",
+        "1300px": "1300px",
+        "400px": "400px",
+      },
     },
   },
+  plugins: [],
 };
-export const plugins = [];
