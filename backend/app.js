71b094854b081a7b29a47a0218227e9fa62c5fae
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-08-22 11:35:14 -0300

    fix: uninstall and remove file-uploader

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,10 @@ const ErrorHandler = require("./utils/ErrorHandler");
 const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const fileUpload = require("express-fileupload");
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(fileUpload({ useTempFiles: true }));
 
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
