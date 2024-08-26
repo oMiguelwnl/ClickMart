df3fefc17a3a6e666d2d5fb2a2b9b590bde68ff9
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-08-26 10:39:07 -0300

    feat: add CORS middleware and replace custom error handler

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const ErrorHandler = require("./utils/ErrorHandler");
+const ErrorHandler = require("./middleware/error");
 const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 app.use("/", express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
