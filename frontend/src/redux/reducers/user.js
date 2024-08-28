add9634eb22c1d34232f825d36ae50a9c03ff8dc
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-08-28 11:48:34 -0300

    feat: add user reducers

---
+++ frontend/src/redux/reducers/user.js
@@ -0,0 +1,24 @@
+import { createReducer } from "@reduxjs/toolkit";
+
+export const initialState = {
+  isAuthenticated: false,
+};
+
+export const userReducer = createReducer(initialState, {
+  LoadUserRequest: (state) => {
+    state.loading = true;
+  },
+  LoadUserSuccess: (state, action) => {
+    state.isAuthenticated = true;
+    state.loading = false;
+    state.user = action.payload;
+  },
+  LoadUserFail: (state, action) => {
+    state.loading = false;
+    state.error = action.payload;
+    state.isAuthenticated = false;
+  },
+  ClearErrors: (state) => {
+    state.error = null;
+  },
+});
