8b04d5d4a55b1ecab6e2bc4dce51fcc5f8799641
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-08-28 16:37:15 -0300

    refactor: update userReducer to use builder syntax

--- frontend/src/redux/reducers/user.js
+++ frontend/src/redux/reducers/user.js
@@ -4,21 +4,22 @@ export const initialState = {
   isAuthenticated: false,
 };
 
-export const userReducer = createReducer(initialState, {
-  LoadUserRequest: (state) => {
-    state.loading = true;
-  },
-  LoadUserSuccess: (state, action) => {
-    state.isAuthenticated = true;
-    state.loading = false;
-    state.user = action.payload;
-  },
-  LoadUserFail: (state, action) => {
-    state.loading = false;
-    state.error = action.payload;
-    state.isAuthenticated = false;
-  },
-  ClearErrors: (state) => {
-    state.error = null;
-  },
+export const userReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase("LoadUserRequest", (state) => {
+      state.loading = true;
+    })
+    .addCase("LoadUserSuccess", (state, action) => {
+      state.isAuthenticated = true;
+      state.loading = false;
+      state.user = action.payload;
+    })
+    .addCase("LoadUserFail", (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+      state.isAuthenticated = false;
+    })
+    .addCase("ClearErrors", (state) => {
+      state.error = null;
+    });
 });
