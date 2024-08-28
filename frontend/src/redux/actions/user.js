12fe8b99b867f3282d94cb6d6a33bd9a92a19cf5
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-08-28 16:36:07 -0300

    refactor: update loadUser action creator to use thunk syntax

--- frontend/src/redux/actions/user.js
+++ frontend/src/redux/actions/user.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 import { server } from "../../server";
 
-export const loadUser = async (dispatch) => {
+export const loadUser = () => async (dispatch) => {
   try {
-    dispatch({
-      type: "LoadUserRequest",
-    });
+    dispatch({ type: "LoadUserRequest" });
+
     const { data } = await axios.get(`${server}/user/getuser`, {
       withCredentials: true,
     });
+
     dispatch({
       type: "LoadUserSuccess",
       payload: data.user,
@@ -16,7 +16,7 @@ export const loadUser = async (dispatch) => {
   } catch (error) {
     dispatch({
       type: "LoadUserFail",
-      payload: error.response.data.message,
+      payload: error.response?.data?.message || "Erro ao carregar usuário",
     });
   }
 };
