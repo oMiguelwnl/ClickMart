19109fca580a670234bc47756481b9b1a42fd73a
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-09-02 15:05:19 -0300

    feat: add fixed header with category dropdown menu

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -2,12 +2,16 @@ import styles from "../../styles/styles";
 import { Link } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { useState } from "react";
-import { productData } from "../../static/data.jsx";
-import { IoIosArrowForward } from "react-icons/io";
+import { productData, categoriesData } from "../../static/data";
+import { IoIosArrowForward, IoIosArrowDown } from "react-icons/io";
+import { BiMenuAltLeft } from "react-icons/bi";
+import DropDown from "./DropDown";
 
 const Header = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchData, setSearchData] = useState(null);
+  const [dropDown, setDropDown] = useState(false);
+  const [active, setActive] = useState(false);
 
   const handleSearchChange = (e) => {
     const term = e.target.value;
@@ -21,62 +25,106 @@ const Header = () => {
 
     setSearchData(filteredProducts);
   };
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+      setActive(true);
+    } else {
+      setActive(false);
+    }
+  });
+
   return (
-    <div className={`${styles.section}`}>
-      <div className="hidden 800px:h-[50px] 800px:my-[20px] 800px:flex items-center justify-between">
-        <div>
-          <Link to="/">
-            <img src="../../../src/assets/logo.png" alt="Logo" />
-          </Link>
-        </div>
-        {/* search box */}
-        <div className="w-[50%] relative">
-          <input
-            type="text"
-            placeholder="Procurar Produto..."
-            value={searchTerm}
-            onChange={handleSearchChange}
-            className="h-[40px] w-full px-2 border-[#3957db] border-[2px] rounded-md"
-          />
-          <AiOutlineSearch
-            size={30}
-            className="absolute right-2 top-1.5 cursor-pointer"
-          />
+    <>
+      <div className={`${styles.section}`}>
+        <div className="hidden 800px:h-[50px] 800px:my-[20px] 800px:flex items-center justify-between">
+          <div>
+            <Link to="/">
+              <img src="../../../src/assets/logo.png" alt="Logo" />
+            </Link>
+          </div>
+          {/* search box */}
+          <div className="w-[50%] relative">
+            <input
+              type="text"
+              placeholder="Procurar Produto..."
+              value={searchTerm}
+              onChange={handleSearchChange}
+              className="h-[40px] w-full px-2 border-[#3957db] border-[2px] rounded-md"
+            />
+            <AiOutlineSearch
+              size={30}
+              className="absolute right-2 top-1.5 cursor-pointer"
+            />
 
-          {searchData && searchData.length !== 0 ? (
-            <div className="absolute min-h-[30vh] bg-slate-50 shadow-sm-2 z-[9] p-4">
-              {searchData &&
-                searchData.map((i, index) => {
-                  const d = i.name;
+            {searchData && searchData.length !== 0 ? (
+              <div className="absolute min-h-[30vh] bg-slate-50 shadow-sm-2 z-[9] p-4">
+                {searchData &&
+                  searchData.map((i, index) => {
+                    const d = i.name;
 
-                  const Product_name = d.replace(/\s+/g, "-");
+                    const Product_name = d.replace(/\s+/g, "-");
 
-                  return (
-                    <Link to={`/product/${Product_name}`} key={index}>
-                      <div className="w-full flex items-start py-3 ">
-                        <img
-                          src={i.image_Url[0].url}
-                          alt="product"
-                          className="w-[40px] h-[40px] mr-[10px]"
-                        />
-                        <h1>{i.name}</h1>
-                      </div>
-                    </Link>
-                  );
-                })}
-            </div>
-          ) : null}
-        </div>
+                    return (
+                      <Link to={`/product/${Product_name}`} key={index}>
+                        <div className="w-full flex items-start py-3 ">
+                          <img
+                            src={i.image_Url[0].url}
+                            alt="product"
+                            className="w-[40px] h-[40px] mr-[10px]"
+                          />
+                          <h1>{i.name}</h1>
+                        </div>
+                      </Link>
+                    );
+                  })}
+              </div>
+            ) : null}
+          </div>
 
-        <div className={`${styles.button} w-[165px]`}>
-          <Link to="/seller">
-            <h1 className="text-[#fff] flex items-center">
-              Torne-se Vendedor <IoIosArrowForward className="ml-1" />
-            </h1>
-          </Link>
+          <div className={`${styles.button} w-[165px]`}>
+            <Link to="/seller">
+              <h1 className="text-[#fff] flex items-center">
+                Torne-se Vendedor <IoIosArrowForward className="ml-1" />
+              </h1>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div
+        className={`${
+          active === true ? "shadow-sm fixed top-0 left-0 z-10" : null
+        }transition hidden 800px:flex items-center justify-between w-full bg-[#3321c8] h-[70px]`}
+      >
+        <div
+          className={`${styles.section} relative ${styles.normalFlex} justify-between`}
+        >
+          {/* Category Menu */}
+          <div onClick={() => setDropDown(!dropDown)}>
+            <div className="relative h-[60px] mt-[10px] w-[270px] hidden 1000px:block">
+              <BiMenuAltLeft size={30} className="absolute top-3 left-2" />
+              <button
+                className={`h-[100%] w-full flex justify-between items-center pl-10 bg-white font-sans text-lg font-[500] select-none rounded-t-md`}
+              >
+                Todas Categorias
+              </button>
+              <IoIosArrowDown
+                size={20}
+                className="absolute right-2 top-4 cursor-pointer"
+                onClick={() => setDropDown(!dropDown)}
+              />
+              {dropDown ? (
+                <DropDown
+                  categoriesData={categoriesData}
+                  setDropDown={setDropDown}
+                />
+              ) : null}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
