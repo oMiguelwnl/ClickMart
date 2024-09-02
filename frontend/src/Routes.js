c7b6bb87c415db04842ccf26dbf23455a53717d9
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-09-02 10:47:34 -0300

    feat: add HomePage to the routes export list

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,5 +1,6 @@
 import LoginPage from "../src/pages/Login";
 import SignUpPage from "./pages/SignUpPage";
-import ActivationPage from "./pages/ActivationPage.jsx";
+import ActivationPage from "./pages/ActivationPage";
+import HomePage from "./pages/HomePage";
 
-export { LoginPage, SignUpPage, ActivationPage };
+export { LoginPage, SignUpPage, ActivationPage, HomePage };
