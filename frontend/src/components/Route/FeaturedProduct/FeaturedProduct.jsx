553479220c331e68e7b36dfb80943a25680edbf3
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-09-04 09:46:48 -0300

    fix: correct JSX mapping for ProductCard component in FeaturedProduct

--- frontend/src/components/Route/FeaturedProduct/FeaturedProduct.jsx
+++ frontend/src/components/Route/FeaturedProduct/FeaturedProduct.jsx
@@ -12,9 +12,7 @@ const FeaturedProduct = () => {
 
         <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
           {productData &&
-            productData.map((i, index) => {
-              <ProductCard key={index} data={i} />;
-            })}
+            productData.map((i, index) => <ProductCard data={i} key={index} />)}
         </div>
       </div>
     </div>
