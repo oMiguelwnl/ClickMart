215f1cf9873bfb13901c32d04a222f0870ad8541
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-09-06 15:48:02 -0300

    feat: add EventsPage to routes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -4,6 +4,7 @@ import ActivationPage from "./pages/ActivationPage";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
 import BestSellingPage from "./pages/BestSellingPage";
+import EventsPage from "./pages/EventsPage";
 
 export {
   LoginPage,
@@ -12,4 +13,5 @@ export {
   HomePage,
   ProductsPage,
   BestSellingPage,
+  EventsPage,
 };
