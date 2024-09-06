35092a8011ca9eeb88822b110c18c1abb5c6573f
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-09-06 11:30:59 -0300

    feat: add ProductsPage to routes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,5 +2,6 @@ import LoginPage from "../src/pages/Login";
 import SignUpPage from "./pages/SignUpPage";
 import ActivationPage from "./pages/ActivationPage";
 import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
 
-export { LoginPage, SignUpPage, ActivationPage, HomePage };
+export { LoginPage, SignUpPage, ActivationPage, HomePage, ProductsPage };
