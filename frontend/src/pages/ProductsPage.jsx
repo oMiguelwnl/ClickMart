d31c2d785c9c7b7716d826e015a1ad1e8dfafad3
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-09-06 15:36:20 -0300

    feat: add Products page

---
+++ frontend/src/pages/ProductsPage.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import Header from "../components/Layout/Header";
+import styles from "../styles/styles";
+import { productData } from "../static/data";
+import ProductCard from "../components/Route/ProductCard/ProductCard";
+import Footer from "../components/Layout/Footer";
+
+const ProductsPage = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const d =
+      productData && productData.sort((a, b) => a.total_sell - b.total_sell);
+    setData(d);
+  }, []);
+
+  return (
+    <div>
+      <Header activeHeading={3} />
+      <br />
+      <br />
+      <div className={`${styles.section}`}>
+        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
+          {data && data.map((i, index) => <ProductCard data={i} key={index} />)}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default ProductsPage;
