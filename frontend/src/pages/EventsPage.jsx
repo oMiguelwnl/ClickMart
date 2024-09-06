8db9c305f8ffada8681dd7d976503a30b301ee0c
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-09-06 15:48:25 -0300

    feat: add EventsPage

---
+++ frontend/src/pages/EventsPage.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Layout/Header";
+import EventCard from "../components/Route/Events/EventCard";
+
+const EventsPage = () => {
+  return (
+    <div>
+      <Header activeHeading={4} />
+      <EventCard active={true} />
+      <EventCard active={true} />
+    </div>
+  );
+};
+
+export default EventsPage;
