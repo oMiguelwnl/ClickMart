29fd5b72483a2af6a662d6c8b0cca6897a63e650
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-09-06 16:09:23 -0300

    feat: add FaqPage

---
+++ frontend/src/pages/FaqPage.jsx
@@ -0,0 +1,331 @@
+import { useState } from "react";
+import Footer from "../components/Layout/Footer";
+import Header from "../components/Layout/Header";
+import styles from "../styles/styles";
+
+const FAQPage = () => {
+  return (
+    <div>
+      <Header activeHeading={5} />
+      <Faq />
+      <Footer />
+    </div>
+  );
+};
+
+const Faq = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const toggleTab = (tab) => {
+    if (activeTab === tab) {
+      setActiveTab(0);
+    } else {
+      setActiveTab(tab);
+    }
+  };
+
+  return (
+    <div className={`${styles.section} my-8`}>
+      <h2 className="text-3xl font-bold text-gray-900 mb-8">FAQ</h2>
+      <div className="mx-auto space-y-4">
+        {/* single Faq */}
+        <div className="border-b border-gray-200 pb-4">
+          <button
+            className="flex items-center justify-between w-full"
+            onClick={() => toggleTab(2)}
+          >
+            <span className="text-lg font-medium text-gray-900">
+              Qual é a sua política de devolução?
+            </span>
+            {activeTab === 2 ? (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            )}
+          </button>
+          {activeTab === 2 && (
+            <div className="mt-4">
+              <p className="text-base text-gray-500">
+                Se você não estiver satisfeito com sua compra, aceitamos
+                devoluções em até 30 dias após a entrega. Para iniciar a
+                devolução, envie um e-mail para support@myecommercestore.com com
+                o número do pedido e uma breve explicação do motivo da
+                devolução.
+              </p>
+            </div>
+          )}
+        </div>
+
+        <div className="border-b border-gray-200 pb-4">
+          <button
+            className="flex items-center justify-between w-full"
+            onClick={() => toggleTab(3)}
+          >
+            <span className="text-lg font-medium text-gray-900">
+              Como faço para rastrear meu pedido?
+            </span>
+            {activeTab === 3 ? (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            )}
+          </button>
+          {activeTab === 3 && (
+            <div className="mt-4">
+              <p className="text-base text-gray-500">
+                Você pode rastrear seu pedido clicando no link de rastreamento
+                no e-mail de confirmação de envio ou acessando sua conta no
+                nosso site e visualizando os detalhes do pedido.
+              </p>
+            </div>
+          )}
+        </div>
+
+        <div className="border-b border-gray-200 pb-4">
+          <button
+            className="flex items-center justify-between w-full"
+            onClick={() => toggleTab(4)}
+          >
+            <span className="text-lg font-medium text-gray-900">
+              Como entro em contato com o suporte ao cliente?
+            </span>
+            {activeTab === 4 ? (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            )}
+          </button>
+          {activeTab === 4 && (
+            <div className="mt-4">
+              <p className="text-base text-gray-500">
+                Você pode entrar em contato com nosso suporte ao cliente
+                enviando um e-mail para support@myecommercestore.com ou ligando
+                para (555) 123-4567 entre 9h e 17h EST, de segunda a
+                sexta-feira.
+              </p>
+            </div>
+          )}
+        </div>
+
+        <div className="border-b border-gray-200 pb-4">
+          <button
+            className="flex items-center justify-between w-full"
+            onClick={() => toggleTab(5)}
+          >
+            <span className="text-lg font-medium text-gray-900">
+              Posso alterar ou cancelar meu pedido?
+            </span>
+            {activeTab === 5 ? (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            )}
+          </button>
+          {activeTab === 5 && (
+            <div className="mt-4">
+              <p className="text-base text-gray-500">
+                Infelizmente, uma vez que o pedido foi realizado, não podemos
+                fazer alterações ou cancelamentos. Se você não quiser mais os
+                itens, poderá devolvê-los para reembolso em até 30 dias após a
+                entrega.
+              </p>
+            </div>
+          )}
+        </div>
+
+        <div className="border-b border-gray-200 pb-4">
+          <button
+            className="flex items-center justify-between w-full"
+            onClick={() => toggleTab(6)}
+          >
+            <span className="text-lg font-medium text-gray-900">
+              Vocês oferecem envio internacional?
+            </span>
+            {activeTab === 6 ? (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            )}
+          </button>
+          {activeTab === 6 && (
+            <div className="mt-4">
+              <p className="text-base text-gray-500">
+                No momento, oferecemos envio apenas dentro dos Estados Unidos.
+              </p>
+            </div>
+          )}
+        </div>
+
+        <div className="border-b border-gray-200 pb-4">
+          <button
+            className="flex items-center justify-between w-full"
+            onClick={() => toggleTab(7)}
+          >
+            <span className="text-lg font-medium text-gray-900">
+              Oferecem cartões-presente?
+            </span>
+            {activeTab === 7 ? (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            )}
+          </button>
+          {activeTab === 7 && (
+            <div className="mt-4">
+              <p className="text-base text-gray-500">
+                Sim, oferecemos cartões-presente disponíveis para compra em
+                nosso site.
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FAQPage;
