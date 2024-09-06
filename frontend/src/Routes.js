6044396daa901c53380bb071e63141958015af55
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-09-06 15:35:34 -0300

    feat: add BestSellingPage to Routes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,5 +3,13 @@ import SignUpPage from "./pages/SignUpPage";
 import ActivationPage from "./pages/ActivationPage";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
+import BestSellingPage from "./pages/BestSellingPage";
 
-export { LoginPage, SignUpPage, ActivationPage, HomePage, ProductsPage };
+export {
+  LoginPage,
+  SignUpPage,
+  ActivationPage,
+  HomePage,
+  ProductsPage,
+  BestSellingPage,
+};
