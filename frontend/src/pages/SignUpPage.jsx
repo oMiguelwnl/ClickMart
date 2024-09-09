891d4200fab9d2da3cf6c72423f9c19efcbb62f3
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-09-09 09:59:04 -0300

    feat: redirect authenticated users from SignUpPage

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import SignUp from "../components/SignUp/SignUp";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const SignUpPage = () => {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (isAuthenticated === true) {
+      navigate("/");
+    }
+  }, [isAuthenticated, navigate]);
+
   return (
     <div>
       <SignUp />
