6287ddd29f91aac37db4a0c04415a6118eb7e3f1
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-09-09 09:59:49 -0300

    feat: redirect authenticated users from Login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,18 @@
+import { useSelector } from "react-redux";
 import Login from "../components/Login/Login";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (isAuthenticated === true) {
+      navigate("/");
+    }
+  }, [isAuthenticated, navigate]);
+
   return (
     <div>
       <Login />
