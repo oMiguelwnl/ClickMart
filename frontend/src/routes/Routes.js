df7172d848ec162f71065bad992fd32df6e585e8
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-09-11 12:09:15 -0300

    chore: organize routes by moving them to the routes folder

---
+++ frontend/src/routes/Routes.js
@@ -0,0 +1,21 @@
+import LoginPage from "../pages/Login";
+import SignUpPage from "../pages/SignUpPage";
+import ActivationPage from "../pages/ActivationPage";
+import HomePage from "../pages/HomePage";
+import ProductsPage from "../pages/ProductsPage";
+import BestSellingPage from "../pages/BestSellingPage";
+import EventsPage from "../pages/EventsPage";
+import FaqPage from "../pages/FaqPage";
+import ProductDetailsPage from "../pages/ProductDetailsPage";
+
+export {
+  LoginPage,
+  SignUpPage,
+  ActivationPage,
+  HomePage,
+  ProductsPage,
+  BestSellingPage,
+  EventsPage,
+  FaqPage,
+  ProductDetailsPage,
+};
