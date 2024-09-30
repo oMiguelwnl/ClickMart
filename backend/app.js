17c3efe77796c6e0076ca799cb4f32934e0f6ae7
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-09-30 10:55:46 -0300

    feat: add shop controller to app

--- backend/app.js
+++ backend/app.js
@@ -26,8 +26,10 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 }
 
 const user = require("./controller/user");
+const shop = require("./controller/shop");
 
 app.use("/api/v2/user", user);
+app.use("/api/v2/shop", shop);
 
 app.use(ErrorHandler);
 
