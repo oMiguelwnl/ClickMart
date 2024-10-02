b5550b02a2a3d9fe6e0cee3b4838209c9e5f2b67
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-02 10:52:52 -0300

    feat: add ShopLoginPage

---
+++ frontend/src/pages/ShopLoginPage.jsx
@@ -0,0 +1,11 @@
+import ShopLogin from "../components/Shop/ShopLogin";
+
+const ShopLoginPage = () => {
+  return (
+    <div>
+      <ShopLogin />
+    </div>
+  );
+};
+
+export default ShopLoginPage;
