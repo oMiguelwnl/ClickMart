a0916b2a1e08e423b3999a5f3edd422cf4eab485
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-02 10:52:01 -0300

    feat: add ShopLogin to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import {
   ProfilePage,
   ShopCreate,
   SellerActivationPage,
+  ShopLogin,
 } from "./routes/Routes.js";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -59,6 +60,7 @@ function App() {
               }
             />
             <Route path="/shop-create" element={<ShopCreate />} />
+            <Route path="/shop-login" element={<ShopLogin />} />
           </Routes>
           <ToastContainer
             position="bottom-center"
