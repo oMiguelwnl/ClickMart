dadb616c3a4a612cfba9308014bd7f4e60db9293
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-03 15:57:26 -0300

    feat: add DashboardSideBar component

---
+++ frontend/src/components/Shop/Layout/DashboardSideBar.jsx
@@ -0,0 +1,197 @@
+import { Link } from "react-router-dom";
+import { RxDashboard } from "react-icons/rx";
+import { FiPackage, FiShoppingBag } from "react-icons/fi";
+import { AiOutlineFolderAdd, AiOutlineGift } from "react-icons/ai";
+import { MdOutlineLocalOffer } from "react-icons/md";
+import { VscNewFile } from "react-icons/vsc";
+import { CiMoneyBill } from "react-icons/ci";
+import { BiMessageSquareDetail } from "react-icons/bi";
+import { HiOutlineReceiptRefund } from "react-icons/hi";
+import { CiSettings } from "react-icons/ci";
+
+const DashboardSideBar = ({ active }) => {
+  return (
+    <div className="w-full h-[90vh] bg-white shadow-sm overflow-y-scroll sticky top-0 left-0 z-10">
+      <div className="w-full flex items-center p-4">
+        <Link to="/dashboard" className="w-full flex items-center">
+          <RxDashboard
+            size={30}
+            color={`${active === 1 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 1 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Dashboard
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link to="/dashboard-pedidos" className="w-full flex items-center">
+          <FiShoppingBag
+            size={30}
+            color={`${active === 2 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 2 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Todos os Pedidos
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link to="/dashboard-produtos" className="w-full flex items-center">
+          <FiPackage size={30} color={`${active === 3 ? "crimson" : "#555"}`} />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 3 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Todos os Produtos
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link
+          to="/dashboard-criar-produto"
+          className="w-full flex items-center"
+        >
+          <AiOutlineFolderAdd
+            size={30}
+            color={`${active === 4 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 4 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Criar Produto
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link to="/dashboard-eventos" className="w-full flex items-center">
+          <MdOutlineLocalOffer
+            size={30}
+            color={`${active === 5 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 5 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Todos os Eventos
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link to="/dashboard-criar-evento" className="w-full flex items-center">
+          <VscNewFile
+            size={30}
+            color={`${active === 6 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 6 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Criar Evento
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link
+          to="/dashboard-retirar-dinheiro"
+          className="w-full flex items-center"
+        >
+          <CiMoneyBill
+            size={30}
+            color={`${active === 7 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 7 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Retirar Dinheiro
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link to="/dashboard-mensagens" className="w-full flex items-center">
+          <BiMessageSquareDetail
+            size={30}
+            color={`${active === 8 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 8 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Caixa de Entrada da Loja
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link to="/dashboard-cupons" className="w-full flex items-center">
+          <AiOutlineGift
+            size={30}
+            color={`${active === 9 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 9 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Códigos de Desconto
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link to="/dashboard-reembolsos" className="w-full flex items-center">
+          <HiOutlineReceiptRefund
+            size={30}
+            color={`${active === 10 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 10 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Reembolsos
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link to="/configuracoes" className="w-full flex items-center">
+          <CiSettings
+            size={30}
+            color={`${active === 11 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 11 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Configurações
+          </h5>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardSideBar;
