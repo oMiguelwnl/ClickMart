6f1918fdb28769d08e12b09c90df61867cca9ea0
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-03 15:18:15 -0300

    feat: add ShopDashboardPage with Dashboard Header

---
+++ frontend/src/pages/Shop/ShopDashboardPage.jsx
@@ -0,0 +1,11 @@
+import DashboardHeader from "../../components/Shop/Layout/DashboardHeader";
+
+const ShopDashboardPage = () => {
+  return (
+    <div>
+      <DashboardHeader />
+    </div>
+  );
+};
+
+export default ShopDashboardPage;
