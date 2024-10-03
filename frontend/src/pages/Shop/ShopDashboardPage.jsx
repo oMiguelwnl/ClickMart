bd0dc9adf7de48c342d11713181ac8368defceae
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-03 15:56:51 -0300

    feat: add SideBar on Dashboard

--- frontend/src/pages/Shop/ShopDashboardPage.jsx
+++ frontend/src/pages/Shop/ShopDashboardPage.jsx
@@ -1,9 +1,15 @@
 import DashboardHeader from "../../components/Shop/Layout/DashboardHeader";
+import DashboardSideBar from "../../components/Shop/Layout/DashboardSideBar";
 
 const ShopDashboardPage = () => {
   return (
     <div>
       <DashboardHeader />
+      <div className="flex items-start justify-between w-full">
+        <div className="w-[80px] 800px:w-[330px]">
+          <DashboardSideBar active={1} />
+        </div>
+      </div>
     </div>
   );
 };
