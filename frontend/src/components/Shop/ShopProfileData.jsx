831114d73ba2afce3dd085d7d0288f88e139f65d
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-04 10:46:49 -0300

    feat: add ShopProfileData component

---
+++ frontend/src/components/Shop/ShopProfileData.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import ProductCard from "../Route/ProductCard/ProductCard";
+import { productData } from "../../static/data";
+import styles from "../../styles/styles";
+
+const ShopProfileData = ({ isOwner }) => {
+  const [active, setActive] = useState(1);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <div className="w-full flex">
+          <div className="flex items-center" onClick={() => setActive(1)}>
+            <h5
+              className={`font-[600] text-[20px] ${
+                active === 1 ? "text-red-500" : "text-[#333]"
+              } cursor-pointer pr-[20px]`}
+            >
+              Produtos da Loja
+            </h5>
+          </div>
+
+          <div className="flex items-center" onClick={() => setActive(2)}>
+            <h5
+              className={`font-[600] text-[20px] ${
+                active === 2 ? "text-red-500" : "text-[#333]"
+              } cursor-pointer pr-[20px]`}
+            >
+              Eventos Atuais
+            </h5>
+          </div>
+
+          <div className="flex items-center" onClick={() => setActive(3)}>
+            <h5
+              className={`font-[600] text-[20px] ${
+                active === 3 ? "text-red-500" : "text-[#333]"
+              } cursor-pointer pr-[20px]`}
+            >
+              Avaliações da Loja
+            </h5>
+          </div>
+        </div>
+
+        <div>
+          {isOwner && (
+            <div>
+              <Link to="/dashboard">
+                <div className={`${styles.button} !rounded-[4px] h-[42px]`}>
+                  <span className="text-[#fff]">Ir para o Dashboard</span>
+                </div>
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+
+      <br />
+      {active === 1 && (
+        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] xl:grid-cols-4 xl:gap-[20px] mb-12 border-0">
+          {productData &&
+            productData.map((i, index) => <ProductCard data={i} key={index} />)}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ShopProfileData;
