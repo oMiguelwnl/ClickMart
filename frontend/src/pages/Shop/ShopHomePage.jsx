b69a8c2f2ae53d5237120757c07b9d72231014e0
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-04 10:45:47 -0300

    feat: add ShopHomePage with ShopInfo and ShopProfileData

--- frontend/src/pages/Shop/ShopHomePage.jsx
+++ frontend/src/pages/Shop/ShopHomePage.jsx
@@ -1,5 +1,20 @@
+import styles from "../../styles/styles";
+import ShopInfo from "../../components/Shop/ShopInfo";
+import ShopProfileData from "../../components/Shop/ShopProfileData";
+
 const ShopHomePage = () => {
-  return <div>ShopHomePage</div>;
+  return (
+    <div className={`${styles.section} bg-[#f5f5f5]`}>
+      <div className="flex w-full justify-between py-10">
+        <div className="w-[25%] bg-[#fff] rounded-[4px] shadow-sm overflow-y-scroll h-[90vh] sticky top-10 left-0 z-10">
+          <ShopInfo isOwner={true} />
+        </div>
+        <div className="w-[72%] rounded-[4px]">
+            <ShopProfileData isOwner={true} />
+          </div>
+      </div>
+    </div>
+  );
 };
 
 export default ShopHomePage;
