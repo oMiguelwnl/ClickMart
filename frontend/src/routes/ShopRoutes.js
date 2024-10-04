544f9dca359d5a3b7f4b609c335c4aa82e4355a3
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-04 11:54:11 -0300

    feat: add ShopCreateProductPage to ShopRoutes

--- frontend/src/routes/ShopRoutes.js
+++ frontend/src/routes/ShopRoutes.js
@@ -1,4 +1,5 @@
 import ShopDashboardPage from "../pages/Shop/ShopDashboardPage";
 import ShopHomePage from "../pages/Shop/ShopHomePage";
+import ShopCreateProductPage from "../pages/Shop/ShopCreateProductPage";
 
-export { ShopDashboardPage, ShopHomePage };
+export { ShopDashboardPage, ShopHomePage, ShopCreateProductPage };
