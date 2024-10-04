fc9a0558c8322aad235bfcea53e5907378227679
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-04 16:03:45 -0300

    feat: add product controller

---
+++ backend/controller/product.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const router = express.Router();
+const Product = require("../model/product");
+const Shop = require("../model/shop");
+const cloudinary = require("cloudinary");
+const ErrorHandler = require("../utils/ErrorHandler");
+
+router.post(
+  "/create-product",
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const shopId = req.body.shopId;
+      const shop = await Shop.findById(shopId);
+      if (!shop) {
+        return next(new ErrorHandler("Loja não encontrada", 400));
+      } else {
+        let images = [];
+
+        if (typeof req.body.images === "string") {
+          images.push(req.body.images);
+        } else {
+          images = req.body.images;
+        }
+
+        const imagesLinks = [];
+
+        for (let i = 0; i < images.length; i++) {
+          const result = await cloudinary.v2.uploader.upload(images[i], {
+            folder: "products",
+          });
+
+          imagesLinks.push({
+            public_id: result.public_id,
+            url: result.secure_url,
+          });
+        }
+
+        const productData = req.body;
+        productData.images = imagesLinks;
+        productData.shop = shop;
+
+        const product = await Product.create(productData);
+
+        res.status(201).json({
+          success: true,
+          product,
+        });
+      }
+    } catch (error) {
+      return next(new ErrorHandler(error, 400));
+    }
+  })
+);
+
+module.exports = router;
