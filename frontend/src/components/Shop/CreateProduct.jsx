1b73f997311d7ec0f3739430a4cf3ad25da243a9
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-04 11:55:04 -0300

    feat: add CreateProduct component

---
+++ frontend/src/components/Shop/CreateProduct.jsx
@@ -0,0 +1,186 @@
+import { useState } from "react";
+import { categoriesData } from "../../static/data";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import { useSelector } from "react-redux";
+
+const CreateProduct = () => {
+  const { seller } = useSelector((state) => state.seller);
+
+  const [images, setImages] = useState([]);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [tags, setTags] = useState("");
+  const [originalPrice, setOriginalPrice] = useState();
+  const [discountPrice, setDiscountPrice] = useState();
+  const [stock, setStock] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleImageChange = (e) => {
+    e.preventDefault();
+
+    let files = Array.from(e.target.files);
+    setImages((prevImages) => [...prevImages, ...files]);
+  };
+
+  return (
+    <div className="w-[90%] 800px:w-[50%] bg-white shadow h-[80vh] rounded-[4px] p-3 overflow-y-scroll">
+      <h5 className="text-[30px] font-poppins text-center">Criar Produto</h5>
+
+      <form onSubmit={handleSubmit}>
+        <br />
+        <div>
+          <label className="pb-2">
+            Nome <span className="text-red-500">*</span>
+          </label>
+          <input
+            type="text"
+            name="nome"
+            value={name}
+            className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Digite o nome do seu produto..."
+          />
+        </div>
+        <br />
+
+        <div>
+          <label className="pb-2">
+            Descrição <span className="text-red-500">*</span>
+          </label>
+          <textarea
+            cols="30"
+            required
+            rows="8"
+            type="text"
+            name="descricao"
+            value={description}
+            className="mt-2 appearance-none block w-full pt-2 px-3 border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder="Digite a descrição do seu produto..."
+          ></textarea>
+        </div>
+
+        <br />
+        <div>
+          <label className="pb-2">
+            Categoria <span className="text-red-500">*</span>
+          </label>
+          <select
+            className="w-full mt-2 border h-[35px] rounded-[5px]"
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="Selecione uma categoria">
+              Selecione uma categoria
+            </option>
+            {categoriesData &&
+              categoriesData.map((i) => (
+                <option value={i.title} key={i.title}>
+                  {i.title}
+                </option>
+              ))}
+          </select>
+        </div>
+
+        <br />
+        <div>
+          <label className="pb-2">Tags</label>
+          <input
+            type="text"
+            name="tags"
+            value={tags}
+            className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            onChange={(e) => setTags(e.target.value)}
+            placeholder="Digite as tags do seu produto..."
+          />
+        </div>
+
+        <br />
+        <div>
+          <label className="pb-2">Preço Original</label>
+          <input
+            type="number"
+            name="preço"
+            value={originalPrice}
+            className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            onChange={(e) => setOriginalPrice(e.target.value)}
+            placeholder="Digite o preço original do produto..."
+          />
+        </div>
+
+        <br />
+        <div>
+          <label className="pb-2">
+            Preço (Com disconto) <span className="text-red-500">*</span>
+          </label>
+          <input
+            type="number"
+            name="preço"
+            value={discountPrice}
+            className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            onChange={(e) => setDiscountPrice(e.target.value)}
+            placeholder="Digite o preço com o desconto..."
+          />
+        </div>
+
+        <br />
+        <div>
+          <label className="pb-2">
+            Estoque do Produto <span className="text-red-500">*</span>
+          </label>
+          <input
+            type="number"
+            name="price"
+            value={stock}
+            className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            onChange={(e) => setStock(e.target.value)}
+            placeholder="Digite o estoque do produto..."
+          />
+        </div>
+
+        <br />
+        <div>
+          <label className="pb-2">
+            Enviar imagens <span className="text-red-500">*</span>
+          </label>
+          <input
+            type="file"
+            name=""
+            id="upload"
+            className="hidden"
+            multiple
+            onChange={handleImageChange}
+          />
+          <div className="w-full flex items-center flex-wrap">
+            <label htmlFor="upload">
+              <AiOutlinePlusCircle size={30} className="mt-3" color="#555" />
+            </label>
+            {images &&
+              images.map((i) => (
+                <img
+                  src={i}
+                  key={i}
+                  alt="imagens"
+                  className="h-[120px] w-[120px] object-cover m-2"
+                />
+              ))}
+          </div>
+        </div>
+
+        <br />
+        <div>
+          <input
+            type="submit"
+            value="Criar Produto"
+            className="mt-2 cursor-pointer appearance-none text-center block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateProduct;
