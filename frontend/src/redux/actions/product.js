7ee32e59d5a53a78be621761458ce98b19ddee9c
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-08 15:03:28 -0300

    feat: add product action

---
+++ frontend/src/redux/actions/product.js
@@ -0,0 +1,27 @@
+import { server } from "../../server";
+import axios from "axios";
+
+export const createProduct = (newForm) => async (dispatch) => {
+  try {
+    dispatch({
+      type: "productCreateRequest",
+    });
+
+    const config = { header: { "Content-Type": "multipart/form-data" } };
+    const { data } = await axios.post(
+      `${server}/product/create-product`,
+      newForm,
+      config
+    );
+
+    dispatch({
+      type: "productCreateSuccess",
+      payload: data.product,
+    });
+  } catch (error) {
+    dispatch({
+      type: "productCreateFail",
+      payload: error.response.data.message,
+    });
+  }
+};
