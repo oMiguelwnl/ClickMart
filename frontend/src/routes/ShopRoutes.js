0c92bbde700621adcfa7485e11d6e2e661133a52
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-09 10:50:36 -0300

    feat: add ShopAllProductsPage to ShopRoutes

--- frontend/src/routes/ShopRoutes.js
+++ frontend/src/routes/ShopRoutes.js
@@ -1,5 +1,11 @@
 import ShopDashboardPage from "../pages/Shop/ShopDashboardPage";
 import ShopHomePage from "../pages/Shop/ShopHomePage";
 import ShopCreateProductPage from "../pages/Shop/ShopCreateProductPage";
+import ShopAllProductsPage from "../pages/Shop/ShopAllProductsPage";
 
-export { ShopDashboardPage, ShopHomePage, ShopCreateProductPage };
+export {
+  ShopDashboardPage,
+  ShopHomePage,
+  ShopCreateProductPage,
+  ShopAllProductsPage,
+};
