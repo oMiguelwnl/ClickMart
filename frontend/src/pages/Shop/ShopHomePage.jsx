615789f566327d0b6881abd78fd86819cb3dbb71
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-09 15:08:56 -0300

    fix: update ShopHomePage format

--- frontend/src/pages/Shop/ShopHomePage.jsx
+++ frontend/src/pages/Shop/ShopHomePage.jsx
@@ -5,13 +5,13 @@ import ShopProfileData from "../../components/Shop/ShopProfileData";
 const ShopHomePage = () => {
   return (
     <div className={`${styles.section} bg-[#f5f5f5]`}>
-      <div className="flex w-full justify-between py-10">
+      <div className="w-full flex py-10 justify-between">
         <div className="w-[25%] bg-[#fff] rounded-[4px] shadow-sm overflow-y-scroll h-[90vh] sticky top-10 left-0 z-10">
           <ShopInfo isOwner={true} />
         </div>
         <div className="w-[72%] rounded-[4px]">
-            <ShopProfileData isOwner={true} />
-          </div>
+          <ShopProfileData isOwner={true} />
+        </div>
       </div>
     </div>
   );
