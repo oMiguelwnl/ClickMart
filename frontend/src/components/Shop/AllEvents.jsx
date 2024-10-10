c5d449cfdb77ec59c54d4dabec1db5044bbf398a
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-10 15:40:57 -0300

    feat: add AllEvents component

---
+++ frontend/src/components/Shop/AllEvents.jsx
@@ -0,0 +1,115 @@
+import { useEffect } from "react";
+import { AiOutlineDelete, AiOutlineEye } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { DataGrid } from "@mui/x-data-grid";
+import Button from "@mui/material/Button";
+import { deleteEvent, getAllEventsShop } from "../../redux/actions/event";
+import Loader from "../Layout/Loader";
+
+const AllEvents = () => {
+  const { events, isLoading } = useSelector((state) => state.events);
+  const { seller } = useSelector((state) => state.seller);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllEventsShop(seller._id));
+  }, [dispatch, seller._id]);
+
+  const handleDelete = (id) => {
+    dispatch(deleteEvent(id));
+  };
+
+  const columns = [
+    { field: "id", headerName: "ID do Produto", minWidth: 150, flex: 0.7 },
+    {
+      field: "name",
+      headerName: "Nome",
+      minWidth: 180,
+      flex: 1.4,
+    },
+    {
+      field: "price",
+      headerName: "Preço",
+      minWidth: 100,
+      flex: 0.6,
+    },
+    {
+      field: "Stock",
+      headerName: "Estoque",
+      type: "number",
+      minWidth: 80,
+      flex: 0.5,
+    },
+    {
+      field: "sold",
+      headerName: "Vendido",
+      type: "number",
+      minWidth: 130,
+      flex: 0.6,
+    },
+    {
+      field: "Preview",
+      flex: 0.8,
+      minWidth: 100,
+      headerName: "Visualização",
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        const productName = params.row.name.replace(/\s+/g, "-");
+
+        return (
+          <Link to={`/produto/${productName}`}>
+            <Button>
+              <AiOutlineEye size={20} />
+            </Button>
+          </Link>
+        );
+      },
+    },
+    {
+      field: "Delete",
+      flex: 0.8,
+      minWidth: 120,
+      headerName: "Excluir",
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <Button onClick={() => handleDelete(params.id)}>
+            <AiOutlineDelete size={20} />
+          </Button>
+        );
+      },
+    },
+  ];
+
+  const rows = events?.map((item) => ({
+    id: item._id,
+    name: item.name,
+    price: `R$ ${item.discountPrice}`,
+    Stock: item.stock,
+    sold: 10,
+  }));
+
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="w-full mx-8 pt-1 mt-10 bg-white">
+          <DataGrid
+            rows={rows || []}
+            columns={columns}
+            pageSizeOptions={[10]}
+            disableSelectionOnClick
+            autoHeight
+          />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AllEvents;
