a4bc3b94feeaf03362c2fecc67850ca0f804777d
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-10 11:00:44 -0300

    feat: add Event actions

---
+++ frontend/src/redux/actions/event.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import { server } from "../../server";
+
+export const createEvent = (data) => async (dispatch) => {
+  try {
+    dispatch({
+      type: "eventCreateRequest",
+    });
+
+    const { d } = await axios.post(`${server}/event/create-event`, data);
+    dispatch({
+      type: "eventCreateSuccess",
+      payload: d.event,
+    });
+  } catch (error) {
+    dispatch({
+      type: "eventCreateFail",
+      payload: error.response.data.message,
+    });
+  }
+};
+
+export const getAllEventsShop = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: "getAllEventsShopRequest",
+    });
+
+    const { data } = await axios.get(`${server}/event/get-all-events/${id}`);
+    dispatch({
+      type: "getAllEventsShopSuccess",
+      payload: data.events,
+    });
+  } catch (error) {
+    dispatch({
+      type: "getAllEventsShopFailed",
+      payload: error.response.data.message,
+    });
+  }
+};
+
+export const deleteEvent = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: "deleteEventRequest",
+    });
+
+    const { data } = await axios.delete(
+      `${server}/event/delete-shop-event/${id}`,
+      {
+        withCredentials: true,
+      }
+    );
+
+    dispatch({
+      type: "deleteEventSuccess",
+      payload: data.message,
+    });
+  } catch (error) {
+    dispatch({
+      type: "deleteEventFailed",
+      payload: error.response.data.message,
+    });
+  }
+};
