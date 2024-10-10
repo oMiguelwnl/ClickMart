b72f3e459f756582a844c61f439fad016ff04359
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-10 11:00:21 -0300

    feat: add Event reducer

---
+++ frontend/src/redux/reducers/event.js
@@ -0,0 +1,49 @@
+import { createReducer } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoading: true,
+};
+
+export const eventReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase("eventCreateRequest", (state) => {
+      state.isLoading = true;
+    })
+    .addCase("eventCreateSuccess", (state, action) => {
+      state.isLoading = false;
+      state.event = action.payload;
+      state.success = true;
+    })
+    .addCase("eventCreateFail", (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+      state.success = false;
+    })
+    .addCase("getAllEventsShopRequest", (state) => {
+      state.isLoading = true;
+    })
+    .addCase("getAllEventsShopSuccess", (state, action) => {
+      state.isLoading = false;
+      state.events = action.payload;
+    })
+    .addCase("getAllEventsShopFailed", (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    })
+
+    // delete event
+    .addCase("deleteEventRequest", (state) => {
+      state.isLoading = true;
+    })
+    .addCase("deleteEventSuccess", (state, action) => {
+      state.isLoading = false;
+      state.message = action.payload;
+    })
+    .addCase("deleteEventFailed", (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    })
+    .addCase("ClearErrors", (state) => {
+      state.error = null;
+    });
+});
