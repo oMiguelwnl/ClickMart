f859ec3b9445c0daf7dbdb53f941eee30377e1ad
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-10 11:02:05 -0300

    feat: add CreateEvent component

---
+++ frontend/src/components/Shop/CreateEvent.jsx
@@ -0,0 +1,279 @@
+import { useEffect, useState } from "react";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { categoriesData } from "../../static/data";
+import { toast } from "react-toastify";
+import { createEvent } from "../../redux/actions/event";
+
+const CreateEvent = () => {
+  const { seller } = useSelector((state) => state.seller);
+  const { success, error } = useSelector((state) => state.events);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [images, setImages] = useState([]);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [tags, setTags] = useState("");
+  const [originalPrice, setOriginalPrice] = useState();
+  const [discountPrice, setDiscountPrice] = useState();
+  const [stock, setStock] = useState();
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
+  const handleStartDateChange = (e) => {
+    const startDate = new Date(e.target.value);
+    const minEndDate = new Date(startDate.getTime() + 3 * 24 * 60 * 60 * 1000);
+    setStartDate(startDate);
+    setEndDate(null);
+    document.getElementById("end-date").min = minEndDate.toISOString.slice(
+      0,
+      10
+    );
+  };
+
+  const handleEndDateChange = (e) => {
+    const endDate = new Date(e.target.value);
+    setEndDate(endDate);
+  };
+
+  const today = new Date().toISOString().slice(0, 10);
+
+  const minEndDate = startDate
+    ? new Date(startDate.getTime() + 3 * 24 * 60 * 60 * 1000)
+        .toISOString()
+        .slice(0, 10)
+    : "";
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+    }
+    if (success) {
+      toast.success("Evento criado com sucesso!");
+      navigate("/dashboard-eventos");
+      window.location.reload();
+    }
+  }, [dispatch, error, success]);
+
+  const handleImageChange = (e) => {
+    const files = Array.from(e.target.files);
+
+    setImages([]);
+
+    files.forEach((file) => {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        if (reader.readyState === 2) {
+          setImages((old) => [...old, reader.result]);
+        }
+      };
+      reader.readAsDataURL(file);
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newForm = new FormData();
+
+    images.forEach((image) => {
+      newForm.append("images", image);
+    });
+    const data = {
+      name,
+      description,
+      category,
+      tags,
+      originalPrice,
+      discountPrice,
+      stock,
+      images,
+      shopId: seller._id,
+      start_Date: startDate?.toISOString(),
+      finish_Date: endDate?.toISOString(),
+    };
+    dispatch(createEvent(data));
+  };
+
+  return (
+    <div className="w-[90%] 800px:w-[50%] bg-white  shadow h-[80vh] rounded-[4px] p-3 overflow-y-scroll">
+      <h5 className="text-[30px] font-Poppins text-center">Criar Evento</h5>
+      <form onSubmit={handleSubmit}>
+        <br />
+        <div>
+          <label className="pb-2">
+            Nome <span className="text-red-500">*</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Insira o nome do produto do evento..."
+          />
+        </div>
+        <br />
+        <div>
+          <label className="pb-2">
+            Descrição <span className="text-red-500">*</span>
+          </label>
+          <textarea
+            cols="30"
+            required
+            rows="8"
+            type="text"
+            name="description"
+            value={description}
+            className="mt-2 appearance-none block w-full pt-2 px-3 border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder="Insira a descrição do produto do evento..."
+          ></textarea>
+        </div>
+        <br />
+        <div>
+          <label className="pb-2">
+            Categoria <span className="text-red-500">*</span>
+          </label>
+          <select
+            className="w-full mt-2 border h-[35px] rounded-[5px]"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="Choose a category">Escolha uma categoria</option>
+            {categoriesData &&
+              categoriesData.map((i) => (
+                <option value={i.title} key={i.title}>
+                  {i.title}
+                </option>
+              ))}
+          </select>
+        </div>
+        <br />
+        <div>
+          <label className="pb-2">Tags</label>
+          <input
+            type="text"
+            name="tags"
+            value={tags}
+            className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            onChange={(e) => setTags(e.target.value)}
+            placeholder="Insira as tags do produto do evento..."
+          />
+        </div>
+        <br />
+        <div>
+          <label className="pb-2">Preço Original</label>
+          <input
+            type="number"
+            name="price"
+            value={originalPrice}
+            className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            onChange={(e) => setOriginalPrice(e.target.value)}
+            placeholder="Insira o preço do produto do evento..."
+          />
+        </div>
+        <br />
+        <div>
+          <label className="pb-2">
+            Preço (Com Desconto) <span className="text-red-500">*</span>
+          </label>
+          <input
+            type="number"
+            name="price"
+            value={discountPrice}
+            className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            onChange={(e) => setDiscountPrice(e.target.value)}
+            placeholder="Insira o preço do produto do evento com desconto..."
+          />
+        </div>
+        <br />
+        <div>
+          <label className="pb-2">
+            Estoque de Produtos <span className="text-red-500">*</span>
+          </label>
+          <input
+            type="number"
+            name="price"
+            value={stock}
+            className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            onChange={(e) => setStock(e.target.value)}
+            placeholder="Insira o estoque do produto do evento..."
+          />
+        </div>
+        <br />
+        <div>
+          <label className="pb-2">
+            Data de Início do Evento <span className="text-red-500">*</span>
+          </label>
+          <input
+            type="date"
+            name="price"
+            id="start-date"
+            value={startDate ? startDate.toISOString().slice(0, 10) : ""}
+            className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            onChange={handleStartDateChange}
+            min={today}
+            placeholder="Insira a data de início do evento..."
+          />
+        </div>
+        <br />
+        <div>
+          <label className="pb-2">
+            Data de Término do Evento <span className="text-red-500">*</span>
+          </label>
+          <input
+            type="date"
+            name="price"
+            id="start-date"
+            value={endDate ? endDate.toISOString().slice(0, 10) : ""}
+            className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            onChange={handleEndDateChange}
+            min={minEndDate}
+            placeholder="Insira a data de término do evento..."
+          />
+        </div>
+        <br />
+        <div>
+          <label className="pb-2">Escolha as Imagens</label>
+          <input
+            type="file"
+            name=""
+            id="upload"
+            className="hidden"
+            multiple
+            onChange={handleImageChange}
+          />
+          <div className="w-full flex items-center flex-wrap">
+            <label htmlFor="upload">
+              <AiOutlinePlusCircle size={30} className="mt-3" color="#555" />
+            </label>
+            {images &&
+              images.map((i, index) => (
+                <img
+                  src={i}
+                  key={index}
+                  alt=""
+                  className="h-[120px] w-[120px] object-cover m-2"
+                />
+              ))}
+          </div>
+          <br />
+          <div>
+            <input
+              type="submit"
+              value="Criar Evento"
+              className="mt-2 cursor-pointer appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateEvent;
