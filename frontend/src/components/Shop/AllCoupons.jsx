5cef446a88d67616505b8ae1f40b828fb93465c2
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-15 16:51:17 -0300

    feat: add AllCoupons component

---
+++ frontend/src/components/Shop/AllCoupons.jsx
@@ -0,0 +1,251 @@
+import { Button } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { AiOutlineDelete } from "react-icons/ai";
+import { RxCross1 } from "react-icons/rx";
+import { useDispatch, useSelector } from "react-redux";
+import styles from "../../styles/styles";
+import Loader from "../Layout/Loader";
+import { server } from "../../server";
+import { toast } from "react-toastify";
+
+const AllCoupons = () => {
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [coupons, setCoupons] = useState([]);
+  const [minAmount, setMinAmount] = useState(null);
+  const [maxAmount, setMaxAmount] = useState(null);
+  const [selectedProducts, setSelectedProducts] = useState(null);
+  const [value, setValue] = useState(null);
+
+  const { seller } = useSelector((state) => state.seller);
+  const { products } = useSelector((state) => state.products);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get(`${server}/coupon/get-coupon/${seller._id}`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setIsLoading(false);
+        setCoupons(res.data.coupons);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        toast.error("Falha ao carregar os cupons");
+      });
+  }, [dispatch, seller._id]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios
+      .post(
+        `${server}/coupon/create-coupon-code`,
+        {
+          name,
+          minAmount,
+          maxAmount,
+          selectedProducts,
+          value,
+          shopId: seller._id,
+        },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        toast.success("Cupom criado com sucesso!");
+        setOpen(false);
+        setCoupons([...coupons, res.data.couponCode]);
+        window.location.reload();
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+      });
+  };
+
+  const handleDelete = async (id) => {
+    axios
+      .delete(`${server}/coupon/delete-coupon/${id}`, { withCredentials: true })
+      .then(() => {
+        toast.success("Cupom excluído com sucesso!");
+        setCoupons(coupons.filter((coupon) => coupon._id !== id));
+        window.location.reload();
+      })
+      .catch(() => {
+        toast.error("Falha ao excluir o cupom");
+      });
+  };
+
+  const columns = [
+    { field: "id", headerName: "Id", minWidth: 150, flex: 0.7 },
+    {
+      field: "name",
+      headerName: "Código do Cupom",
+      minWidth: 180,
+      flex: 1.4,
+    },
+    {
+      field: "price",
+      headerName: "Valor",
+      minWidth: 100,
+      flex: 0.6,
+    },
+    {
+      field: "Delete",
+      flex: 0.8,
+      minWidth: 120,
+      headerName: "",
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+            <Button onClick={() => handleDelete(params.id)}>
+              <AiOutlineDelete size={20} />
+            </Button>
+          </>
+        );
+      },
+    },
+  ];
+
+  const rows = coupons.map((coupon) => ({
+    id: coupon._id,
+    name: coupon.name,
+    price: coupon.value + " %",
+  }));
+
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="w-full mx-8 pt-1 mt-10 bg-white">
+          <div className="w-full flex justify-end">
+            <div
+              className={`${styles.button} !w-max !h-[45px] px-3 !rounded-[5px] mr-3 mb-3`}
+              onClick={() => setOpen(true)}
+            >
+              <span className="text-white">Criar Código de Cupom</span>
+            </div>
+          </div>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={10}
+            disableSelectionOnClick
+            autoHeight
+          />
+          {open && (
+            <div className="fixed top-0 left-0 w-full h-screen bg-[#00000062] z-[20000] flex items-center justify-center">
+              <div className="w-[90%] 800px:w-[50%] bg-white  shadow h-[80vh] rounded-[4px] p-3 overflow-y-scroll">
+                <div className="w-full flex justify-end">
+                  <RxCross1
+                    size={30}
+                    className="cursor-pointer"
+                    onClick={() => setOpen(false)}
+                  />
+                </div>
+                <h5 className="text-[30px] font-Poppins text-center">
+                  Criar Código de Cupom
+                </h5>
+
+                <form onSubmit={handleSubmit} aria-required={true}>
+                  <br />
+                  <div>
+                    <label className="pb-2">
+                      Nome <span className="text-red-500">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      required
+                      value={name}
+                      className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                      onChange={(e) => setName(e.target.value)}
+                      placeholder="Digite o nome do cupom..."
+                    />
+                  </div>
+                  <br />
+                  <div>
+                    <label className="pb-2">
+                      Porcentagem de Desconto{" "}
+                      <span className="text-red-500">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      name="value"
+                      value={value}
+                      required
+                      className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                      onChange={(e) => setValue(e.target.value)}
+                      placeholder="Digite o valor do desconto..."
+                    />
+                  </div>
+                  <br />
+                  <div>
+                    <label className="pb-2">Valor Mínimo</label>
+                    <input
+                      type="number"
+                      name="value"
+                      value={minAmount}
+                      className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                      onChange={(e) => setMinAmount(e.target.value)}
+                      placeholder="Digite o valor mínimo..."
+                    />
+                  </div>
+                  <br />
+                  <div>
+                    <label className="pb-2">Valor Máximo</label>
+                    <input
+                      type="number"
+                      name="value"
+                      value={maxAmount}
+                      className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                      onChange={(e) => setMaxAmount(e.target.value)}
+                      placeholder="Digite o valor máximo..."
+                    />
+                  </div>
+                  <br />
+                  <div>
+                    <label className="pb-2">Produto Selecionado</label>
+                    <select
+                      className="w-full mt-2 border h-[35px] rounded-[5px]"
+                      value={selectedProducts}
+                      onChange={(e) => setSelectedProducts(e.target.value)}
+                    >
+                      <option value="Escolha o produto">
+                        Escolha um produto
+                      </option>
+                      {products &&
+                        products.map((i) => (
+                          <option value={i.name} key={i.name}>
+                            {i.name}
+                          </option>
+                        ))}
+                    </select>
+                  </div>
+                  <br />
+                  <div>
+                    <input
+                      type="submit"
+                      value="Criar"
+                      className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                    />
+                  </div>
+                </form>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AllCoupons;
