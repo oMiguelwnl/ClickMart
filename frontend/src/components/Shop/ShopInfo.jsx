ed0afe55b523bffcf5a4f7ebde7368a86ee65ddf
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-25 11:46:56 -0300

    refactor: make the ShopInfo component dynamic

--- frontend/src/components/Shop/ShopInfo.jsx
+++ frontend/src/components/Shop/ShopInfo.jsx
@@ -1,11 +1,30 @@
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
 import styles from "../../styles/styles";
 import axios from "axios";
 import { server } from "../../server";
+import { getAllProductsShop } from "../../redux/actions/product";
+import { useEffect, useState } from "react";
+import Loader from "../Layout/Loader";
 
 const ShopInfo = ({ isOwner }) => {
-  const { seller } = useSelector((state) => state.seller);
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`${server}/shop/get-shop-info/${id}`)
+      .then((res) => {
+        setData(res.data.shop);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
+  }, []);
 
   const logoutHandler = () => {
     axios.get(`${server}/shop/logout`, {
@@ -21,65 +40,69 @@ const ShopInfo = ({ isOwner }) => {
 
   return (
     <>
-      <div>
-        <div className="w-full py-5">
-          <div className="w-full flex item-center justify-center">
-            <img
-              src={`${seller.avatar?.url}`}
-              alt="Avatar"
-              className="w-[150px] h-[150px] object-cover rounded-full"
-            />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <div className="w-full py-5">
+            <div className="w-full flex item-center justify-center">
+              <img
+                src={`${data.avatar?.url}`}
+                alt="Avatar"
+                className="w-[150px] h-[150px] object-cover rounded-full"
+              />
+            </div>
+            <h3 className="text-center py-2 text-[20px]">{data.name}</h3>
+            <p className="text-[16px] text-[#000000a6] p-[10px] flex items-center">
+              {data.description}
+            </p>
           </div>
-          <h3 className="text-center py-2 text-[20px]">{seller.name}</h3>
-          <p className="text-[16px] text-[#000000a6] p-[10px] flex items-center">
-            {seller.description}
-          </p>
-        </div>
 
-        <div className="p-3">
-          <h5 className="font-[600]">Endereço</h5>
-          <h4 className="text-[#000000a6]">{seller.address}</h4>
-        </div>
-        <div className="p-3">
-          <h5 className="font-[600]">Número de Telefone</h5>
-          <h4 className="text-[#000000a6]">{seller.phoneNumber}</h4>
-        </div>
-        <div className="p-3">
-          <h5 className="font-[600]">Total de Produtos</h5>
-          <h4 className="text-[#000000a6]">10</h4>
-        </div>
-        <div className="p-3">
-          <h5 className="font-[600]">Avaliação da Loja</h5>
-          <h4 className="text-[#000000b0]">4/5</h4>
-        </div>
+          <div className="p-3">
+            <h5 className="font-[600]">Endereço</h5>
+            <h4 className="text-[#000000a6]">{data.address}</h4>
+          </div>
+          <div className="p-3">
+            <h5 className="font-[600]">Número de Telefone</h5>
+            <h4 className="text-[#000000a6]">{data.phoneNumber}</h4>
+          </div>
+          <div className="p-3">
+            <h5 className="font-[600]">Total de Produtos</h5>
+            <h4 className="text-[#000000a6]">10</h4>
+          </div>
+          <div className="p-3">
+            <h5 className="font-[600]">Avaliação da Loja</h5>
+            <h4 className="text-[#000000b0]">4/5</h4>
+          </div>
 
-        <div className="p-3">
-          <h5 className="font-[600]">Membro Desde</h5>
-          <h4 className="text-[#000000a6]">
-            {seller.createdAt ? formatDate(seller.createdAt) : ""}
-          </h4>
-        </div>
+          <div className="p-3">
+            <h5 className="font-[600]">Membro Desde</h5>
+            <h4 className="text-[#000000a6]">
+              {data.createdAt ? formatDate(data.createdAt) : ""}
+            </h4>
+          </div>
 
-        {isOwner && (
-          <div className="py-3 px-4">
-            <Link to="settings">
+          {isOwner && (
+            <div className="py-3 px-4">
+              <Link to="settings">
+                <div
+                  className={`${styles.button} !w-full !h-[42px] !rounded-[5px]`}
+                >
+                  <span className="text-white">Editar Loja</span>
+                </div>
+              </Link>
+
               <div
                 className={`${styles.button} !w-full !h-[42px] !rounded-[5px]`}
               >
-                <span className="text-white">Editar Loja</span>
+                <span className="text-white" onClick={logoutHandler}>
+                  Sair
+                </span>
               </div>
-            </Link>
-
-            <div
-              className={`${styles.button} !w-full !h-[42px] !rounded-[5px]`}
-            >
-              <span className="text-white" onClick={logoutHandler}>
-                Sair
-              </span>
             </div>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+      )}
     </>
   );
 };
--- frontend/src/components/Shop/ShopProfileData.jsx
+++ frontend/src/components/Shop/ShopProfileData.jsx
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 import { getAllProductsShop } from "../../redux/actions/product";
 import ProductCard from "../Route/ProductCard/ProductCard";
-import { productData } from "../../static/data";
 import styles from "../../styles/styles";
 
 const ShopProfileData = ({ isOwner }) => {
