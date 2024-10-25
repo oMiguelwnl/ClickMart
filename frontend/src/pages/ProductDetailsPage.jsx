6089a9a1e47c7e8896f31a13dbbbbd1318202e2c
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-25 11:44:39 -0300

    refactor: make the ProductDetailsPage dynamic

--- frontend/src/pages/ProductDetailsPage.jsx
+++ frontend/src/pages/ProductDetailsPage.jsx
@@ -1,20 +1,22 @@
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Footer from "../components/Layout/Footer";
 import Header from "../components/Layout/Header";
 import ProductDetails from "../components/Products/ProductDetails";
-import { useState, useEffect } from "react";
-import { productData } from "../static/data";
 import SuggestedProduct from "../components/Products/SuggestedProduct";
+import { useSelector } from "react-redux";
 
 const ProductDetailsPage = () => {
-  const { name } = useParams();
+  const { allProducts } = useSelector((state) => state.products);
+  const { id } = useParams();
   const [data, setData] = useState(null);
-  const productName = name.replace(/-/g, " ");
 
   useEffect(() => {
-    const data = productData.find((i) => i.name === productName);
-    setData(data);
-  }, []);
+    if (allProducts && id) {
+      const product = allProducts.find((i) => i._id === id);
+      setData(product);
+    }
+  }, [allProducts, id]);
 
   return (
     <div>
