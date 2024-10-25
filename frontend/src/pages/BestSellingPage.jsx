73eb0058b7b3afd540fb5c13ea6c2ecd2f1608f9
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-25 11:45:12 -0300

    refactor: make the BestSellingPage dynamic

--- frontend/src/pages/BestSellingPage.jsx
+++ frontend/src/pages/BestSellingPage.jsx
@@ -1,46 +1,46 @@
 import { useEffect, useState } from "react";
-import Header from "../components/Layout/Header";
-import styles from "../styles/styles";
+import { useSelector } from "react-redux";
 import { useSearchParams } from "react-router-dom";
-import { productData } from "../static/data";
+import Header from "../components/Layout/Header";
+import Loader from "../components/Layout/Loader";
 import ProductCard from "../components/Route/ProductCard/ProductCard";
+import styles from "../styles/styles";
 import Footer from "../components/Layout/Footer";
 
 const BestSellingPage = () => {
-  const [searchParams] = useSearchParams();
-  const categoryData = searchParams.get("category");
   const [data, setData] = useState([]);
+  const { allProducts, isLoading } = useSelector((state) => state.products);
 
   useEffect(() => {
-    if (categoryData === null) {
-      const d =
-        productData && productData.sort((a, b) => b.total_sell - a.total_sell);
-      setData(d);
-    } else {
-      const d =
-        productData &&
-        productData.filter((item) => item.category === categoryData);
-      setData(d);
-    }
-  }, []);
+    const allProductsData = allProducts ? [...allProducts] : [];
+    const sortedData = allProductsData?.sort((a, b) => b.sold_out - a.sold_out);
+    setData(sortedData);
+  }, [allProducts]);
 
   return (
-    <div>
-      <Header activeHeading={2} />
-      <br />
-      <br />
-      <div className={`${styles.section}`}>
-        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
-          {data && data.map((i, index) => <ProductCard data={i} key={index} />)}
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <Header activeHeading={2} />
+          <br />
+          <br />
+          <div className={`${styles.section}`}>
+            <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
+              {data &&
+                data.map((i, index) => <ProductCard data={i} key={index} />)}
+            </div>
+            {data && data.length === 0 ? (
+              <h1 className="text-center w-full pb-[100px] text-[20px]">
+                Nenhum produto encontrado!
+              </h1>
+            ) : null}
+          </div>
+          <Footer />
         </div>
-        {data && data.length === 0 ? (
-          <h1 className="text-center w-full pb-[100px] text-[20px]">
-            Nenhum produto encontrado!
-          </h1>
-        ) : null}
-      </div>
-      <Footer />
-    </div>
+      )}
+    </>
   );
 };
 
