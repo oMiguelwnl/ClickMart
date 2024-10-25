2f5d9821503749ccc22960e9b36fae1a02ce4bff
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-25 11:38:26 -0300

    feat: add getAllProducts endpoint in product controller

--- frontend/src/redux/actions/product.js
+++ frontend/src/redux/actions/product.js
@@ -71,3 +71,22 @@ export const deleteProduct = (id) => async (dispatch) => {
     });
   }
 };
+
+export const getAllProducts = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: "getAllProductsRequest",
+    });
+
+    const { data } = await axios.get(`${server}/product/get-all-products`);
+    dispatch({
+      type: "getAllProductsSuccess",
+      payload: data.products,
+    });
+  } catch (error) {
+    dispatch({
+      type: "getAllProductsFailed",
+      payload: error.response.data.message,
+    });
+  }
+};
