cfaa3f6233242761d83f9ea8d97252dd21c3d85c
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-30 16:32:12 -0300

    refactor: make the EventsPage component dynamic

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,13 +1,22 @@
+import { useSelector } from "react-redux";
+import EventCard from "../components/Events/EventCard";
 import Header from "../components/Layout/Header";
-import EventCard from "../components/Route/Events/EventCard";
+import Loader from "../components/Layout/Loader";
 
 const EventsPage = () => {
+  const { allEvents, isLoading } = useSelector((state) => state.events);
+
   return (
-    <div>
-      <Header activeHeading={4} />
-      <EventCard active={true} />
-      <EventCard active={true} />
-    </div>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <Header activeHeading={4} />
+          <EventCard active={true} data={allEvents && allEvents[0]} />
+        </div>
+      )}
+    </>
   );
 };
 
