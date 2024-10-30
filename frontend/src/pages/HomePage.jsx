8d7534c63a18e37e34b18df6fa2c086b18cd05da
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-30 16:31:30 -0300

    fix: update Events route

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,9 +3,9 @@ import Hero from "../components/Route/Hero";
 import Categories from "../components/Route/Categories/Categories";
 import BestDeals from "../components/Route/BestDeals/BestDeals";
 import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct";
-import Events from "../components/Route/Events/Events";
 import Sponsored from "../components/Route/Sponsored";
 import Footer from "../components/Layout/Footer";
+import Events from "../components/Events/Events";
 
 const HomePage = () => {
   return (
