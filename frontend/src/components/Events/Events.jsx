e3fc93f9509d05f96c77c15ad6ba52d762b903c3
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-30 16:34:48 -0300

    refactor: make the Events component dynamic

---
+++ frontend/src/components/Events/Events.jsx
@@ -0,0 +1,27 @@
+import { useSelector } from "react-redux";
+import styles from "../../styles/styles";
+import EventCard from "./EventCard";
+
+const Events = () => {
+  const { allEvents, isLoading } = useSelector((state) => state.events);
+
+  return (
+    <div>
+      {!isLoading && (
+        <div className={`${styles.section}`}>
+          <div className={`${styles.heading}`}>
+            <h1>Eventos Populares</h1>
+          </div>
+
+          <div className="w-full grid">
+            {allEvents.length !== 0 && (
+              <EventCard data={allEvents && allEvents[0]} />
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Events;
