49a690c4052039d55fb59ff2ab67abe6f1f9507f
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-10-31 16:21:59 -0300

    feat: add Cart Reducers

---
+++ frontend/src/redux/reducers/cart.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: localStorage.getItem("cartItems")
+    ? JSON.parse(localStorage.getItem("cartItems"))
+    : [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      const isItemExist = state.cart.find((i) => i._id === item._id);
+      if (isItemExist) {
+        state.cart = state.cart.map((i) => (i._id === isItemExist._id ? item : i));
+      } else {
+        state.cart.push(item);
+      }
+      localStorage.setItem("cartItems", JSON.stringify(state.cart)); // Atualizando localStorage aqui
+    },
+    removeFromCart: (state, action) => {
+      state.cart = state.cart.filter((i) => i._id !== action.payload);
+      localStorage.setItem("cartItems", JSON.stringify(state.cart)); // Atualizando localStorage aqui
+    },
+  },
+});
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
+export const cartReducer = cartSlice.reducer;
