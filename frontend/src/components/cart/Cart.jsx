b570bf5f0a11c05575cd510632a5c7b824eb4e7d
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-01 15:36:57 -0300

    refactor: improve cart component structure and update quantity management logic

--- frontend/src/components/cart/Cart.jsx
+++ frontend/src/components/cart/Cart.jsx
@@ -1,74 +1,111 @@
 import { RxCross1 } from "react-icons/rx";
 import styles from "../../styles/styles";
 import { IoBagHandleOutline } from "react-icons/io5";
-import { useState } from "react";
 import { HiOutlineMinus, HiPlus } from "react-icons/hi";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import { addToCart, removeFromCart } from "../../redux/reducers/cart";
 
 const Cart = ({ setOpenCard }) => {
-  const cartData = [
-    {
-      name: "Iphone 14 pro max 256 gb 8gb ram sliver color",
-      description: "test",
-      price: 1000,
-    },
-    {
-      name: "Iphone 14 pro max 256 gb 8gb ram sliver color",
-      description: "test",
-      price: 1234,
-    },
-    {
-      name: "Iphone 14 pro max 256 gb 8gb ram sliver color",
-      description: "test",
-      price: 2356,
-    },
-  ];
+  const { cart } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const removeFromCartHandler = (data) => {
+    dispatch(removeFromCart(data._id));
+  };
+
+  const totalPrice = cart.reduce(
+    (acc, item) => acc + item.qty * item.discountPrice,
+    0
+  );
+
+  const quantityChangeHandler = (data) => {
+    dispatch(addToCart(data));
+  };
+
   return (
-    <div className="fixed top-0 left-0 w-full bg-[#0000004b] h-screen z-10">
-      <div className="fixed top-0 right-0 min-h-full w-[25%] bg-white flex flex-col justify-between shadow-sm">
-        <div>
-          <div className="flex w-full justify-end pt-5 pr-5">
-            <RxCross1
-              size={25}
-              className="cursor-pointer"
-              onClick={() => setOpenCard(false)}
-            />
+    <div className="fixed top-0 left-0 w-full bg-[#0000004b] h-screen z-10 flex justify-end">
+      <div className="fixed top-0 right-0 h-full w-full sm:w-[50%] md:w-[40%] lg:w-[30%] bg-white flex flex-col justify-between shadow-sm overflow-y-auto">
+        {cart && cart.length === 0 ? (
+          <div className="w-full h-full flex items-center justify-center">
+            <div className="flex w-full justify-end pt-5 pr-5">
+              <RxCross1
+                size={25}
+                className="cursor-pointer"
+                onClick={() => setOpenCard(false)}
+              />
+            </div>
+            <h5>Carrinho está vazio!</h5>
           </div>
-
-          {/* Item Length */}
-          <div className={`${styles.normalFlex} p-4`}>
-            <IoBagHandleOutline size={25} />
-            <h5 className="pl-2 text-[20px] font-[500]">3 items</h5>
-          </div>
-
-          {/* Cart Single Item */}
-          <br />
-          <div className="w-full border-t">
-            {cartData &&
-              cartData.map((i, index) => <CartSingle key={index} data={i} />)}
-          </div>
-
-          <div className="px-5 mb-3">
-            {/* checkout buttons */}
-            <Link to="/checkout">
-              <div
-                className={`h-[45px] flex items-center justify-center w-[100%] bg-[#e44343] rounded-[5px]`}
-              >
-                <h1 className="text-[#fff] text-[18px] font-[600]">
-                  Finalizar compra (R$ 10.000)
-                </h1>
+        ) : (
+          <>
+            <div>
+              <div className="flex w-full justify-end pt-5 pr-5">
+                <RxCross1
+                  size={25}
+                  className="cursor-pointer"
+                  onClick={() => setOpenCard(false)}
+                />
               </div>
-            </Link>
-          </div>
-        </div>
+
+              <div className="flex items-center p-4 border-b">
+                <IoBagHandleOutline size={25} />
+                <h5 className="pl-2 text-[20px] font-[500]">
+                  {cart && cart.length} itens
+                </h5>
+              </div>
+
+              <div className="p-4 space-y-4 overflow-y-auto max-h-[60vh]">
+                {cart &&
+                  cart.map((item, index) => (
+                    <CartSingle
+                      key={index}
+                      data={item}
+                      quantityChangeHandler={quantityChangeHandler}
+                      removeFromCartHandler={removeFromCartHandler}
+                    />
+                  ))}
+              </div>
+            </div>
+            <div className="px-4 py-3 bg-gray-50 border-t">
+              <Link to="/checkout">
+                <div className="flex items-center justify-center w-full bg-[#e44343] rounded-[5px] h-[45px] md:h-[50px] text-white font-semibold text-[16px] md:text-[18px]">
+                  Finalizar compra (R$ {totalPrice})
+                </div>
+              </Link>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
 };
 
-const CartSingle = ({ data }) => {
-  const [value, setValue] = useState(1);
-  const totalPrice = data.price * value;
+const CartSingle = ({ data, quantityChangeHandler, removeFromCartHandler }) => {
+  const [value, setValue] = useState(data.qty);
+  const [totalPrice, setTotalPrice] = useState(data.discountPrice * value);
+
+  useEffect(() => {
+    setTotalPrice(data.discountPrice * value);
+  }, [value, data.discountPrice]);
+
+  const increment = () => {
+    if (data.stock <= value) {
+      toast.error("Produto com estoque limitado!");
+    } else {
+      const newValue = value + 1;
+      setValue(newValue);
+      quantityChangeHandler({ ...data, qty: newValue });
+    }
+  };
+
+  const decrement = () => {
+    const newValue = value === 1 ? 1 : value - 1;
+    setValue(newValue);
+    quantityChangeHandler({ ...data, qty: newValue });
+  };
 
   return (
     <div className="border-b p-4">
@@ -76,36 +113,36 @@ const CartSingle = ({ data }) => {
         <div>
           <div
             className={`bg-[#e44343] border border-[#e4434373] rounded-full w-[25px] h-[25px] ${styles.normalFlex} justify-center cursor-pointer`}
-            onClick={() => setValue(value + 1)}
+            onClick={increment}
           >
             <HiPlus size={18} color="#fff" />
           </div>
-
           <span className="pl-[10px]">{value}</span>
-
           <div
             className="bg-[#a7abb14f] rounded-full w-[25px] h-[25px] flex items-center justify-center cursor-pointer"
-            onClick={() => setValue(value === 1 ? 1 : value - 1)}
+            onClick={decrement}
           >
             <HiOutlineMinus size={16} color="#7d879c" />
           </div>
         </div>
         <img
-          src="https://bonik-react.vercel.app/assets/images/products/Fashion/Clothes/1.SilverHighNeckSweater.png"
-          alt="produto"
+          src={`${data?.images[0]?.url}`}
+          alt=""
           className="w-[130px] h-min ml-2 mr-2 rounded-[5px]"
         />
-
         <div className="pl-[5px]">
-          <h2>{data.name}</h2>
+          <h1>{data.name}</h1>
           <h4 className="font-[400] text-[15px] text-[#00000082]">
-            R${data.price} * {value}
+            ${data.discountPrice} * {value}
           </h4>
           <h4 className="font-[600] text-[17px] pt-[3px] text-[#d02222] font-Roboto">
-            R${totalPrice}
+            R${totalPrice.toFixed(2)}
           </h4>
         </div>
-        <RxCross1 className="cursor-pointer" />
+        <RxCross1
+          className="cursor-pointer"
+          onClick={() => removeFromCartHandler(data)}
+        />
       </div>
     </div>
   );
