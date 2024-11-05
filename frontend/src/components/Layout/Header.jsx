a45d0742056431c815dcf7b0b6345647fa877623
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-05 09:56:41 -0300

    refactor: update product link structure to use ID instead of name-based URL

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -79,12 +79,8 @@ const Header = ({ activeHeading }) => {
               <div className="absolute min-h-[30vh] bg-slate-50 shadow-sm-2 z-[9] p-4">
                 {searchData &&
                   searchData.map((i, index) => {
-                    const d = i.name;
-
-                    const Product_name = d.replace(/\s+/g, "-");
-
                     return (
-                      <Link to={`/produto/${Product_name}`} key={index}>
+                      <Link to={`/produto/${i._id}`} key={index}>
                         <div className="w-full flex items-start py-3 ">
                           <img
                             src={`${i.images[0]?.url}`}
