99e90e321dead8f9e0693d0cf5da46447b3357f8
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-05 10:47:13 -0300

    feat: add UpdateUser endpoint

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -170,4 +170,35 @@ router.get(
   })
 );
 
+router.put("/update-user-info", isAuthenticated, catchAsyncErrors(async (req, res, next) => {
+  try {
+    const { name, email, phoneNumber, password } = req.body;
+
+    const user = await User.findById(req.user._id).select("+password");
+
+    if (!user) {
+      return next(new ErrorHandler("Usuário não existe", 400));
+    }
+
+    const isPasswordValid = await user.comparePassword(password);
+    if (!isPasswordValid) {
+      return next(new ErrorHandler("Por favor, forneça as informações corretas", 400));
+    }
+
+    if (name) user.name = name;
+    if (email) user.email = email;
+    if (phoneNumber) user.phoneNumber = phoneNumber;
+
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Informações atualizadas com sucesso!",
+    });
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+}));
+
+
 module.exports = router;
