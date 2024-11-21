966458df71b7bfd694308160ac5f5d57af36b8a1
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-21 10:53:58 -0300

    feat: add order controller and route to API

--- backend/app.js
+++ backend/app.js
@@ -31,6 +31,7 @@ const product = require("./controller/product");
 const event = require("./controller/event");
 const coupon = require("./controller/coupon");
 const payment = require("./controller/payment");
+const order = require("./controller/order");
 
 app.use("/api/v2/user", user);
 app.use("/api/v2/shop", shop);
@@ -38,6 +39,7 @@ app.use("/api/v2/product", product);
 app.use("/api/v2/event", event);
 app.use("/api/v2/coupon", coupon);
 app.use("/api/v2/payment", payment);
+app.use("/api/v2/order", order);
 
 app.use(ErrorHandler);
 
