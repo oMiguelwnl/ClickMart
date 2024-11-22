c0d17a816209376829736bd4e11fc135338eebe5
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-22 15:32:50 -0300

    feat: add OrderDetails component

---
+++ frontend/src/components/Shop/OrderDetails.jsx
@@ -0,0 +1,212 @@
+import { useEffect, useState } from "react";
+import styles from "../../styles/styles";
+import { BsFillBagFill } from "react-icons/bs";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllOrdersOfShop } from "../../redux/actions/order";
+import { server } from "../../server";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const OrderDetails = () => {
+  const { orders } = useSelector((state) => state.order);
+  const { seller } = useSelector((state) => state.seller);
+
+  const dispatch = useDispatch();
+
+  const [status, setStatus] = useState("");
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    dispatch(getAllOrdersOfShop(seller._id));
+  }, [dispatch]);
+
+  const data = orders && orders.find((item) => item._id === id);
+
+  const orderUpdateHandler = async () => {
+    await axios
+      .put(
+        `${server}/order/update-order-status/${id}`,
+        {
+          status,
+        },
+        { withCredentials: true }
+      )
+      .then(() => {
+        toast.success("Pedido atualizado!");
+        navigate("/dashboard-pedidos");
+      })
+      .catch((error) => {
+        toast.error(error.response.data.message);
+      });
+  };
+
+  const refundOrderUpdateHandler = async () => {
+    await axios
+      .put(
+        `${server}/order/order-refund-success/${id}`,
+        {
+          status,
+        },
+        { withCredentials: true }
+      )
+      .then(() => {
+        toast.success("Pedido atualizado!");
+        dispatch(getAllOrdersOfShop(seller._id));
+      })
+      .catch((error) => {
+        toast.error(error.response.data.message);
+      });
+  };
+
+  return (
+    <div className={`py-4 min-h-screen ${styles.section}`}>
+      <div className="w-full flex items-center justify-between">
+        <div className="flex items-center">
+          <BsFillBagFill size={30} color="crimson" />
+          <h1 className="pl-2 text-[25px]">Detalhes do Pedido</h1>
+        </div>
+        <Link to="/dashboard-pedidos">
+          <div
+            className={`${styles.button} !bg-[#fce1e6] !rounded-[4px] text-[#e94560] font-[600] !h-[45px] text-[18px]`}
+          >
+            Lista de Pedidos
+          </div>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center justify-between pt-6">
+        <h5 className="text-[#00000084]">
+          ID do Pedido: <span>#{data?._id?.slice(0, 8)}</span>
+        </h5>
+        <h5 className="text-[#00000084]">
+          Realizado em:{" "}
+          <span>
+            {data?.createdAt &&
+              `${data.createdAt.slice(8, 10)}/${data.createdAt.slice(
+                5,
+                7
+              )}/${data.createdAt.slice(0, 4)}`}
+          </span>
+        </h5>
+      </div>
+
+      <br />
+      <br />
+      {data &&
+        data?.cart.map((item, index) => (
+          <div className="w-full flex items-start mb-5" key={index}>
+            <img
+              src={`${item.images[0]?.url}`}
+              alt=""
+              className="w-[80x] h-[80px]"
+            />
+            <div className="w-full">
+              <h5 className="pl-3 text-[20px]">{item.name}</h5>
+              <h5 className="pl-3 text-[20px] text-[#00000091]">
+                R${item.discountPrice} x {item.qty}
+              </h5>
+            </div>
+          </div>
+        ))}
+
+      <div className="border-t w-full text-right">
+        <h5 className="pt-3 text-[18px]">
+          Preço Total: <strong>R${data?.totalPrice}</strong>
+        </h5>
+      </div>
+      <br />
+      <br />
+      <div className="w-full 800px:flex items-center">
+        <div className="w-full 800px:w-[60%]">
+          <h4 className="pt-3 text-[20px] font-[600]">Endereço de Entrega:</h4>
+          <h4 className="pt-3 text-[20px]">
+            {data?.shippingAddress.address1 +
+              " " +
+              data?.shippingAddress.address2}
+          </h4>
+          <h4 className=" text-[20px]">{data?.shippingAddress.country}</h4>
+          <h4 className=" text-[20px]">{data?.shippingAddress.city}</h4>
+          <h4 className=" text-[20px]">{data?.user?.phoneNumber}</h4>
+        </div>
+        <div className="w-full 800px:w-[40%]">
+          <h4 className="pt-3 text-[20px]">Informações de Pagamento:</h4>
+          <h4>
+            Status:{" "}
+            {data?.paymentInfo?.status ? data?.paymentInfo?.status : "Não Pago"}
+          </h4>
+        </div>
+      </div>
+      <br />
+      <br />
+      <h4 className="pt-3 text-[20px] font-[600]">Status do Pedido:</h4>
+      {data?.status !== "Processando reembolso" &&
+        data?.status !== "Reembolso Realizado" && (
+          <select
+            value={status}
+            onChange={(e) => setStatus(e.target.value)}
+            className="w-[200px] mt-2 border h-[35px] rounded-[5px]"
+          >
+            {[
+              "Processando",
+              "Transferido para o parceiro de entrega",
+              "Enviando",
+              "Recebido",
+              "A caminho",
+              "Entregue",
+            ]
+              .slice(
+                [
+                  "Processando",
+                  "Transferido para o parceiro de entrega",
+                  "Enviando",
+                  "Recebido",
+                  "A caminho",
+                  "Entregue",
+                ].indexOf(data?.status)
+              )
+              .map((option, index) => (
+                <option value={option} key={index}>
+                  {option}
+                </option>
+              ))}
+          </select>
+        )}
+      {data?.status === "Processando reembolso" ||
+      data?.status === "Reembolso Realizado" ? (
+        <select
+          value={status}
+          onChange={(e) => setStatus(e.target.value)}
+          className="w-[200px] mt-2 border h-[35px] rounded-[5px]"
+        >
+          {["Processando reembolso", "Reembolso Realizado"]
+            .slice(
+              ["Processando reembolso", "Reembolso Realizado"].indexOf(
+                data?.status
+              )
+            )
+            .map((option, index) => (
+              <option value={option} key={index}>
+                {option}
+              </option>
+            ))}
+        </select>
+      ) : null}
+
+      <div
+        className={`${styles.button} mt-5 !bg-[#FCE1E6] !rounded-[4px] text-[#E94560] font-[600] !h-[45px] text-[18px]`}
+        onClick={
+          data?.status !== "Processando reembolso"
+            ? orderUpdateHandler
+            : refundOrderUpdateHandler
+        }
+      >
+        Atualizar Status
+      </div>
+    </div>
+  );
+};
+
+export default OrderDetails;
