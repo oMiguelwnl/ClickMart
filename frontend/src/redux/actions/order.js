ead80080f5fe17d4381fec2d48c372e0dc804877
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-22 10:38:37 -0300

    feat: add getAllOrdersOfShop endpoint

--- frontend/src/redux/actions/order.js
+++ frontend/src/redux/actions/order.js
@@ -22,3 +22,25 @@ export const getAllOrdersOfUser = (userId) => async (dispatch) => {
     });
   }
 };
+
+export const getAllOrdersOfShop = (shopId) => async (dispatch) => {
+  try {
+    dispatch({
+      type: "getAllOrdersShopRequest",
+    });
+
+    const { data } = await axios.get(
+      `${server}/order/get-seller-all-orders/${shopId}`
+    );
+
+    dispatch({
+      type: "getAllOrdersShopSuccess",
+      payload: data.orders,
+    });
+  } catch (error) {
+    dispatch({
+      type: "getAllOrdersShopFailed",
+      payload: error.response.data.message,
+    });
+  }
+};
