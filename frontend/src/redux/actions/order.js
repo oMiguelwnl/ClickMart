cae7e8c30317fc94f092f1ab0e8aaacf56233736
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-22 09:59:47 -0300

    feat: add order action with getAllOrdersOfUser endpoint

---
+++ frontend/src/redux/actions/order.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { server } from "../../server";
+
+export const getAllOrdersOfUser = (userId) => async (dispatch) => {
+  try {
+    dispatch({
+      type: "getAllOrdersUserRequest",
+    });
+
+    const { data } = await axios.get(
+      `${server}/order/get-all-orders/${userId}`
+    );
+
+    dispatch({
+      type: "getAllOrdersUserSuccess",
+      payload: data.orders,
+    });
+  } catch (error) {
+    dispatch({
+      type: "getAllOrdersUserFailed",
+      payload: error.response.data.message,
+    });
+  }
+};
