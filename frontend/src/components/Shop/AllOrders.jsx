73b10ab90f88cd898160c6a272037b30b4d25e29
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-22 15:32:04 -0300

    refactor: fix link endpoint

--- frontend/src/components/Shop/AllOrders.jsx
+++ frontend/src/components/Shop/AllOrders.jsx
@@ -54,7 +54,7 @@ const AllOrders = () => {
       renderCell: (params) => {
         return (
           <>
-            <Link to={`order/${params.id}`}>
+            <Link to={`/order/${params.id}`}>
               <Button>
                 <AiOutlineArrowRight size={20} />
               </Button>
