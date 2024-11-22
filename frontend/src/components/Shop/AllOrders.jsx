b5c3903d5d3d3e2eef327a84f19a8d21309c2386
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-22 10:37:02 -0300

    feat: add AllOrders Component

---
+++ frontend/src/components/Shop/AllOrders.jsx
@@ -0,0 +1,94 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Loader from "../Layout/Loader";
+import { getAllOrdersOfShop } from "../../redux/actions/order";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import { Button } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+
+const AllOrders = () => {
+  const { orders, isLoading } = useSelector((state) => state.order);
+  const { seller } = useSelector((state) => state.seller);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllOrdersOfShop(seller._id));
+  }, [dispatch]);
+
+  const columns = [
+    { field: "id", headerName: "ID do Pedido", minWidth: 150, flex: 0.7 },
+    {
+      field: "status",
+      headerName: "Status",
+      minWidth: 130,
+      flex: 0.7,
+      cellClassName: (params) => {
+        return params.row.status === "Entregue" ? "greenColor" : "redColor";
+      },
+    },
+    {
+      field: "itemsQty",
+      headerName: "Quantidade de Itens",
+      type: "number",
+      minWidth: 130,
+      flex: 0.7,
+    },
+
+    {
+      field: "total",
+      headerName: "Total",
+      type: "number",
+      minWidth: 130,
+      flex: 0.8,
+    },
+
+    {
+      field: " ",
+      flex: 1,
+      minWidth: 150,
+      headerName: "",
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={`order/${params.id}`}>
+              <Button>
+                <AiOutlineArrowRight size={20} />
+              </Button>
+            </Link>
+          </>
+        );
+      },
+    },
+  ];
+
+  const rows = orders?.map((item) => ({
+    id: item._id,
+    itemsQty: item.cart.length,
+    total: "R$ " + item.totalPrice,
+    status: item.status,
+  }));
+
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="w-full mx-8 pt-1 mt-10 bg-white">
+          <DataGrid
+            rows={rows || []}
+            columns={columns}
+            pageSizeOptions={[10]}
+            disableSelectionOnClick
+            autoHeight
+          />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AllOrders;
