49349f8ae4973ce4a0abe80d4094a7c5dfcd07e7
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-22 15:26:29 -0300

    feat: add ShopOrderDetailsPage

---
+++ frontend/src/pages/Shop/ShopOrderDetailsPage.jsx
@@ -0,0 +1,15 @@
+import DashboardHeader from "../../components/Shop/Layout/DashboardHeader";
+import Footer from "../../components/Layout/Footer";
+import OrderDetails from "../../components/Shop/OrderDetails";
+
+const ShopOrderDetailsPage = () => {
+  return (
+    <div>
+      <DashboardHeader />
+      <OrderDetails />
+      <Footer />
+    </div>
+  );
+};
+
+export default ShopOrderDetailsPage;
