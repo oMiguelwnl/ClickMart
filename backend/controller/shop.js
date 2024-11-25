66711446f0edd3e587c0ebfb41c28f589d7763bf
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-25 16:11:32 -0300

    feat: add Update-Seller-Info route to shop controller

--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -227,4 +227,35 @@ router.put(
   })
 );
 
+router.put(
+  "/update-seller-info",
+  isSeller,
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const { name, description, address, phoneNumber, zipCode } = req.body;
+
+      const shop = await Shop.findOne(req.seller._id);
+
+      if (!shop) {
+        return next(new ErrorHandler("Usuário não encontrado", 404));
+      }
+
+      shop.name = name;
+      shop.description = description;
+      shop.address = address;
+      shop.phoneNumber = phoneNumber;
+      shop.zipCode = zipCode;
+
+      await shop.save();
+
+      res.status(201).json({
+        success: true,
+        shop,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  })
+);
+
 module.exports = router;
