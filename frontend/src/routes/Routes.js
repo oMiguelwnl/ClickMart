9979d5aa54c352e39385ca25f0e420a38082ee15
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-25 11:00:56 -0300

    feat: add TrackOrderPage to Routes

--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -15,6 +15,7 @@ import CheckoutPage from "../pages/CheckoutPage";
 import OrderSuccessPage from "../pages/OrderSuccessPage";
 import PaymentPage from "../pages/PaymentPage";
 import OrderDetailsPage from "../pages/OrderDetailsPage";
+import TrackOrderPage from "../pages/TrackOrderPage";
 
 export {
   LoginPage,
@@ -33,5 +34,6 @@ export {
   CheckoutPage,
   OrderSuccessPage,
   PaymentPage,
-  OrderDetailsPage
+  OrderDetailsPage,
+  TrackOrderPage,
 };
