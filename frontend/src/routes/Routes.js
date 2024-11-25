0c6fd3a7092f8be7fac38a221c154a9366c762eb
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-25 09:24:34 -0300

    feat: add OrderDetailsPage to Routes

--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -14,6 +14,7 @@ import SellerActivationPage from "../pages/SellerActivationPage";
 import CheckoutPage from "../pages/CheckoutPage";
 import OrderSuccessPage from "../pages/OrderSuccessPage";
 import PaymentPage from "../pages/PaymentPage";
+import OrderDetailsPage from "../pages/OrderDetailsPage";
 
 export {
   LoginPage,
@@ -32,4 +33,5 @@ export {
   CheckoutPage,
   OrderSuccessPage,
   PaymentPage,
+  OrderDetailsPage
 };
