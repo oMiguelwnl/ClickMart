b0b27d98d5c27df065579bd63fd5ce4264a8b485
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-26 11:10:57 -0300

    feat: add get-all-conversation-user route to conversation controller

--- backend/controller/conversation.js
+++ backend/controller/conversation.js
@@ -35,3 +35,26 @@ router.post(
     }
   })
 );
+
+router.get(
+  "/get-all-conversation-user/:id",
+  isAuthenticated,
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const conversations = await Conversation.find({
+        members: {
+          $in: [req.params.id],
+        },
+      }).sort({ updatedAt: -1, createdAt: -1 });
+
+      res.status(201).json({
+        success: true,
+        conversations,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error), 500);
+    }
+  })
+);
+
+module.exports = router;
