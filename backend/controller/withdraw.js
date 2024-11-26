367a2d18db16b83fd2fcda22db3fa4c8e15cf00c
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-26 10:43:52 -0300

    feat: add withdraw controller with create-withdraw-request route

---
+++ backend/controller/withdraw.js
@@ -0,0 +1,53 @@
+const Shop = require("../model/shop");
+const ErrorHandler = require("../utils/ErrorHandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const express = require("express");
+const { isSeller, isAuthenticated, isAdmin } = require("../middleware/auth");
+const Withdraw = require("../model/withdraw");
+const sendMail = require("../utils/sendMail");
+const router = express.Router();
+
+router.post(
+  "/create-withdraw-request",
+  isSeller,
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const { amount } = req.body;
+
+      const data = {
+        seller: req.seller,
+        amount,
+      };
+
+      try {
+        await sendMail({
+          email: req.seller.email,
+          subject: "Solicitação de Saque",
+          message: `Olá ${req.seller.name}, sua solicitação de saque de ${amount}$ está em processamento. Isso pode levar de 3 a 7 dias para ser processado! `,
+        });
+        res.status(201).json({
+          success: true,
+        });
+      } catch (error) {
+        return next(new ErrorHandler(error.message, 500));
+      }
+
+      const withdraw = await Withdraw.create(data);
+
+      const shop = await Shop.findById(req.seller._id);
+
+      shop.availableBalance = shop.availableBalance - amount;
+
+      await shop.save();
+
+      res.status(201).json({
+        success: true,
+        withdraw,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  })
+);
+
+module.exports = router;
