f9cedcaea25f51c8ec0fe4c22adaa9db18583dd8
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-26 09:33:25 -0300

    feat: add ShopSettingsPage

---
+++ frontend/src/pages/Shop/ShopSettingsPage.jsx
@@ -0,0 +1,19 @@
+import ShopSettings from "../../components/Shop/ShopSettings.jsx";
+import DashboardHeader from "../../components/Shop/Layout/DashboardHeader";
+import DashboardSideBar from "../../components/Shop/Layout/DashboardSideBar.jsx";
+
+const ShopSettingsPage = () => {
+  return (
+    <div>
+      <DashboardHeader />
+      <div className="flex items-start justify-between w-full">
+        <div className="w-[80px] 800px:w-[330px]">
+          <DashboardSideBar active={11} />
+        </div>
+        <ShopSettings />
+      </div>
+    </div>
+  );
+};
+
+export default ShopSettingsPage;
