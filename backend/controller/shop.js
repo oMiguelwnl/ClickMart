a61b3bcc5f99417d27fd9a4e054c2d5256845406
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-26 10:31:42 -0300

    feat: add update-payment-methods route in shop controller

--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -258,4 +258,25 @@ router.put(
   })
 );
 
+router.put(
+  "/update-payment-methods",
+  isSeller,
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const { withdrawMethod } = req.body;
+
+      const seller = await Shop.findByIdAndUpdate(req.seller._id, {
+        withdrawMethod,
+      });
+
+      res.status(201).json({
+        success: true,
+        seller,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  })
+);
+
 module.exports = router;
