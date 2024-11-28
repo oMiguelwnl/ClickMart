1790542683c2fecaec50a0a22e8d4684a1d9e25c
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-28 09:36:21 -0300

    feat: add get-all-conversation-seller and  update-last-message routes to conversation controller

--- backend/controller/conversation.js
+++ backend/controller/conversation.js
@@ -37,6 +37,27 @@ router.post(
 );
 
 router.get(
+  "/get-all-conversation-seller/:id",
+  isSeller,
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const conversations = await Conversation.find({
+        members: {
+          $in: [req.params.id],
+        },
+      }).sort({ updatedAt: -1, createdAt: -1 });
+
+      res.status(201).json({
+        success: true,
+        conversations,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error), 500);
+    }
+  })
+);
+
+router.get(
   "/get-all-conversation-user/:id",
   isAuthenticated,
   catchAsyncErrors(async (req, res, next) => {
@@ -50,6 +71,27 @@ router.get(
       res.status(201).json({
         success: true,
         conversations,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error), 500);
+    }
+  })
+);
+
+router.put(
+  "/update-last-message/:id",
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const { lastMessage, lastMessageId } = req.body;
+
+      const conversation = await Conversation.findByIdAndUpdate(req.params.id, {
+        lastMessage,
+        lastMessageId,
+      });
+
+      res.status(201).json({
+        success: true,
+        conversation,
       });
     } catch (error) {
       return next(new ErrorHandler(error), 500);
