892b19993b7191264d1d4b3f1094806b77ecb414
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-28 09:34:36 -0300

    feat: add message controller to App

--- backend/app.js
+++ backend/app.js
@@ -33,6 +33,7 @@ const coupon = require("./controller/coupon");
 const payment = require("./controller/payment");
 const order = require("./controller/order");
 const conversation = require("./controller/conversation");
+const message = require("./controller/messages");
 
 app.use("/api/v2/user", user);
 app.use("/api/v2/shop", shop);
@@ -42,6 +43,7 @@ app.use("/api/v2/coupon", coupon);
 app.use("/api/v2/payment", payment);
 app.use("/api/v2/order", order);
 app.use("/api/v2/conversation", conversation);
+app.use("/api/v2/message", message);
 
 app.use(ErrorHandler);
 
