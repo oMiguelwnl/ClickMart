8da3133671baaaa79dc23f05d4654766f0f5fbad
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-29 15:10:13 -0300

    feat: add AdminSideBar component

---
+++ frontend/src/pages/components/Admin/Layout/AdminSideBar.jsx
@@ -0,0 +1,142 @@
+import { Link } from "react-router-dom";
+import { RxDashboard } from "react-icons/rx";
+import { FiShoppingBag } from "react-icons/fi";
+import { AiOutlineSetting } from "react-icons/ai";
+import { MdOutlineLocalOffer } from "react-icons/md";
+import { CiMoneyBill } from "react-icons/ci";
+import { BsHandbag } from "react-icons/bs";
+import { GrWorkshop } from "react-icons/gr";
+import { HiOutlineUserGroup } from "react-icons/hi";
+
+const AdminSideBar = ({ active }) => {
+  return (
+    <div className="w-full h-[90vh] bg-white shadow-sm overflow-y-scroll sticky top-0 left-0 z-10">
+      <div className="w-full flex items-center p-4">
+        <Link to="/admin/dashboard" className="w-full flex items-center">
+          <RxDashboard
+            size={30}
+            color={`${active === 1 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 1 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Dashboard
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link to="/admin-pedidos" className="w-full flex items-center">
+          <FiShoppingBag
+            size={30}
+            color={`${active === 2 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 2 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Pedidos
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link to="/admin-vendedores" className="w-full flex items-center">
+          <GrWorkshop
+            size={30}
+            color={`${active === 3 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 3 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Vendedores
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link to="/admin-usuarios" className="w-full flex items-center">
+          <HiOutlineUserGroup
+            size={30}
+            color={`${active === 4 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 4 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Usuários
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link to="/admin-produtos" className="w-full flex items-center">
+          <BsHandbag size={30} color={`${active === 5 ? "crimson" : "#555"}`} />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 5 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Produtos
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link to="/admin-eventos" className="w-full flex items-center">
+          <MdOutlineLocalOffer
+            size={30}
+            color={`${active === 6 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 6 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Eventos
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link to="/admin-sacar-dinheiro" className="w-full flex items-center">
+          <CiMoneyBill
+            size={30}
+            color={`${active === 7 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 7 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Saque
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link to="/admin-configuracoes" className="w-full flex items-center">
+          <AiOutlineSetting
+            size={30}
+            color={`${active === 8 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 8 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Configurações
+          </h5>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default AdminSideBar;
