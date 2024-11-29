5b3a69961b9082e79bb7850209c69fe2db71ad3d
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-29 15:22:03 -0300

    feat: add admin role validation middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -31,3 +31,17 @@ exports.isSeller = catchAsyncErrors(async (req, res, next) => {
 
   next();
 });
+
+exports.isAdmin = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHandler(
+          `${req.user.role} nao tem permissão para acessar estes recursos!`,
+          401
+        )
+      );
+    }
+    next();
+  };
+};
