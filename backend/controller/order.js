c6626862d70e04f283f926f128cf32ebe46b1239
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-29 15:23:20 -0300

    feat: add endpoint for admins to view all orders in order controller

--- backend/controller/order.js
+++ backend/controller/order.js
@@ -198,4 +198,26 @@ router.put(
   })
 );
 
+
+// Admin
+router.get(
+  "/admin-all-orders",
+  isAuthenticated,
+  isAdmin("Admin"),
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const orders = await Order.find().sort({
+        deliveredAt: -1,
+        createdAt: -1,
+      });
+      res.status(201).json({
+        success: true,
+        orders,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  })
+);
+
 module.exports = router;
