728accc4dc3aba07450406483506daa9112852b8
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-29 15:07:03 -0300

    feat: add ProtectedAdminRoute

--- frontend/src/routes/SellerProtectedRoute.jsx
+++ frontend/src/routes/SellerProtectedRoute.jsx
@@ -4,6 +4,7 @@ import Loader from "../components/Layout/Loader";
 
 const SellerProtectedRoute = ({ children }) => {
   const { isLoading, isSeller } = useSelector((state) => state.seller);
+
   if (isLoading === true) {
     return <Loader />;
   } else {
