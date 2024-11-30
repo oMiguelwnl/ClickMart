6830c61d79e080b6391fc507ec06fd8d4d19b504
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-11-30 11:49:44 -0300

    feat: add AdminDashboardOrders to AdminRoutes

--- frontend/src/routes/AdminRoutes.js
+++ frontend/src/routes/AdminRoutes.js
@@ -1,9 +1,11 @@
 import AdminDashboardPage from "../pages/Admin/AdminDashboardPage";
 import AdminDashboardUsersPage from "../pages/Admin/AdminDashboardUsersPage";
 import AdminDashboardSellersPage from "../pages/Admin/AdminDashboardSellersPage";
+import AdminDashboardOrders from "../pages/Admin/AdminDashboardOrders";
 
 export {
   AdminDashboardPage,
   AdminDashboardUsersPage,
   AdminDashboardSellersPage,
+  AdminDashboardOrders,
 };
