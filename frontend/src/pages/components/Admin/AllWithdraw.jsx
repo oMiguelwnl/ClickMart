7e577231f6d8b7935fb4e29b4e9398b4e228a78a
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-12-02 09:47:17 -0300

    feat: add AllWithdraw component

---
+++ frontend/src/pages/components/Admin/AllWithdraw.jsx
@@ -0,0 +1,151 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { BsPencil } from "react-icons/bs";
+import { RxCross1 } from "react-icons/rx";
+import { toast } from "react-toastify";
+import { server } from "../../../server";
+import styles from "../../../styles/styles";
+
+const AllWithdraw = () => {
+  const [data, setData] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [withdrawData, setWithdrawData] = useState();
+  const [withdrawStatus, setWithdrawStatus] = useState("Processando");
+
+  useEffect(() => {
+    axios
+      .get(`${server}/withdraw/get-all-withdraw-request`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setData(res.data.withdraws);
+      })
+      .catch((error) => {
+        console.log(error.response.data.message);
+      });
+  }, []);
+
+  const columns = [
+    { field: "id", headerName: "Id", minWidth: 150, flex: 0.7 },
+    {
+      field: "name",
+      headerName: "Nome da loja",
+      minWidth: 180,
+      flex: 1.4,
+    },
+    {
+      field: "shopId",
+      headerName: "ID da loja",
+      minWidth: 180,
+      flex: 1.4,
+    },
+    {
+      field: "amount",
+      headerName: "Valor",
+      minWidth: 100,
+      flex: 0.6,
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      type: "text",
+      minWidth: 80,
+      flex: 0.5,
+    },
+    {
+      field: "createdAt",
+      headerName: "Requisição dada em",
+      type: "number",
+      minWidth: 130,
+      flex: 0.6,
+    },
+    {
+      field: " ",
+      headerName: "Atualizar Status",
+      type: "number",
+      minWidth: 130,
+      flex: 0.6,
+      renderCell: (params) => {
+        return (
+          <BsPencil
+            size={20}
+            className={`${
+              params.row.status !== "Processando" ? "hidden" : ""
+            } mr-5 cursor-pointer`}
+            onClick={() => setOpen(true) || setWithdrawData(params.row)}
+          />
+        );
+      },
+    },
+  ];
+
+  const handleSubmit = async () => {
+    await axios
+      .put(
+        `${server}/withdraw/update-withdraw-request/${withdrawData.id}`,
+        {
+          sellerId: withdrawData.shopId,
+        },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        toast.success("Withdraw request updated successfully!");
+        setData(res.data.withdraws);
+        setOpen(false);
+      });
+  };
+
+  const rows = data?.map((item) => ({
+    id: item._id,
+    shopId: item.seller._id,
+    name: item.seller.name,
+    amount: "R$ " + item.amount,
+    status: item.status,
+    createdAt: item.createdAt.slice(0, 10),
+  }));
+  return (
+    <div className="w-full flex items-center pt-5 justify-center">
+      <div className="w-[95%] bg-white">
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={10}
+          disableSelectionOnClick
+          autoHeight
+        />
+      </div>
+      {open && (
+        <div className="w-full fixed h-screen top-0 left-0 bg-[#00000031] z-[9999] flex items-center justify-center">
+          <div className="w-[50%] min-h-[40vh] bg-white rounded shadow p-4">
+            <div className="flex justify-end w-full">
+              <RxCross1 size={25} onClick={() => setOpen(false)} />
+            </div>
+            <h1 className="text-[25px] text-center font-Poppins">
+              Atualizar status da solicitação de retirada
+            </h1>
+            <br />
+            <select
+              name=""
+              id=""
+              onChange={(e) => setWithdrawStatus(e.target.value)}
+              className="w-[200px] h-[35px] border rounded"
+            >
+              <option value={withdrawStatus}>{withdrawData.status}</option>
+              <option value={withdrawStatus}>Succeed</option>
+            </select>
+            <button
+              type="submit"
+              className={`block ${styles.button} text-white !h-[42px] mt-4 text-[18px]`}
+              onClick={handleSubmit}
+            >
+              Atualizar
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AllWithdraw;
