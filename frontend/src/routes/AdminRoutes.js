0ef2b18159b505c0df7b46f9a3b34be141ecc915
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-12-02 09:23:20 -0300

    feat: add AdminDashboardEventsPage to AdminRoutes

--- frontend/src/routes/AdminRoutes.js
+++ frontend/src/routes/AdminRoutes.js
@@ -3,6 +3,7 @@ import AdminDashboardUsersPage from "../pages/Admin/AdminDashboardUsersPage";
 import AdminDashboardSellersPage from "../pages/Admin/AdminDashboardSellersPage";
 import AdminDashboardOrders from "../pages/Admin/AdminDashboardOrders";
 import AdminDashboardProductsPage from "../pages/Admin/AdminDashboardProductsPage";
+import AdminDashboardEventsPage from "../pages/Admin/AdminDashboardEventsPage";
 
 export {
   AdminDashboardPage,
@@ -10,4 +11,5 @@ export {
   AdminDashboardSellersPage,
   AdminDashboardOrders,
   AdminDashboardProductsPage,
+  AdminDashboardEventsPage,
 };
