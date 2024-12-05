d3d2a4b873e46843a7fc4eccca56374f386a99be
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-12-05 11:21:57 -0300

    chore: update currency to USD for payment processing

--- backend/controller/payment.js
+++ backend/controller/payment.js
@@ -9,7 +9,7 @@ router.post(
   catchAsyncErrors(async (req, res, next) => {
     const myPayment = await stripe.paymentIntents.create({
       amount: req.body.amount,
-      currency: "brl",
+      currency: "USD",
       metadata: {
         company: "ClickMart",
       },
--- frontend/src/components/Payment/Payment.jsx
+++ frontend/src/components/Payment/Payment.jsx
@@ -36,7 +36,7 @@ const Payment = () => {
           {
             description: "Sunflower",
             amount: {
-              currency_code: "BRL",
+              currency_code: "USD",
               value: orderData?.totalPrice,
             },
           },
