3b4c8ccbcff3c4d552d30b08a6dd5a18a9eca527
oMiguelwnl <contato.miguel.almeida7@gmail.com>
2025-01-25 09:38:18 -0300

    fix: update forgot password link

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -116,7 +116,7 @@ const Login = () => {
               </div>
               <div className="text-sm">
                 <a
-                  href=".forgot-password"
+                  href="/forgot-password"
                   className="font-medium text-blue-600 hover:text-blue-500"
                 >
                   Esqueceu a senha?
--- frontend/src/components/Shop/ShopLogin.jsx
+++ frontend/src/components/Shop/ShopLogin.jsx
@@ -113,7 +113,7 @@ const ShopLogin = () => {
               </div>
               <div className="text-sm">
                 <a
-                  href=".forgot-password"
+                  href="/forgot-password"
                   className="font-medium text-blue-600 hover:text-blue-500"
                 >
                   Esqueceu sua senha?
